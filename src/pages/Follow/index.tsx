a532386d9412842ecd72d220f6b0b51ca2af92a4
gray <516465335@qq.com>
2024-11-07 17:38:59 +0800

    feat(follow): page

---
+++ src/pages/Follow/index.tsx
@@ -0,0 +1,106 @@
+import { FC, useEffect } from 'react'
+import { useStore } from '@/store'
+import { useParams } from 'react-router-dom'
+import { getFollowerList, getFollowingList } from '@/api'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { Spinner } from '@chakra-ui/react'
+import Image from '@/components/Image/Image'
+import FollowButton from '@/components/PersonalDetails/FollowButton'
+import { useTMAUtils } from '@/hooks/useTMAUtils'
+import { Follow } from '@/types'
+const FollowPage: FC = () => {
+  const { uid } = useParams()
+  const searchParams = new URLSearchParams(window.location.search)
+  const type = searchParams.get('type')
+  const title = type === 'follower' ? 'Followers' : 'Following'
+
+  const { launchParams } = useTMAUtils()
+  const currentUid = launchParams.initData?.user?.id ?? 0
+
+  const {
+    token,
+    follower,
+    following,
+    setFollowerList,
+    setFollowingList,
+    resetFollowerList,
+    resetFollowingList,
+  } = useStore((state) => ({
+    token: state.token,
+    follower: state.follower,
+    following: state.following,
+    setFollowerList: state.setFollowerList,
+    setFollowingList: state.setFollowingList,
+    resetFollowerList: state.resetFollowerList,
+    resetFollowingList: state.resetFollowingList,
+  }))
+
+  useEffect(() => {
+    if (uid && token) {
+      if (type === 'follower') {
+        // getFollowerList(Number(uid)).then((res) => setFollowerList(res))
+        setFollowerList(
+          Array.from({ length: 100 }).map((item, index) => ({
+            avatar: 'https://img.bae.boo/christina.jpg',
+            tg_id: 520002 + index,
+            tgname: 'christina1111111111111111111' + index,
+          }))
+        )
+      } else {
+        getFollowingList(Number(uid)).then((res) => setFollowingList(res))
+      }
+    }
+  }, [uid, token])
+
+  useEffect(() => {
+    return () => {
+      resetFollowerList()
+      resetFollowingList()
+    }
+  }, [])
+
+  const FollowItem = ({ item }: { item: Follow }) => {
+    return (
+      <div key={item.tg_id} className="flex items-center h-[80px] my-3">
+        <Image
+          src={item.avatar}
+          alt={item.tgname}
+          width={56}
+          height={56}
+          rect
+          className="rounded-full"
+        />
+        <div className="flex-1 ml-3 truncate overflow-hidden whitespace-nowrap">{item.tgname}</div>
+        <FollowButton
+          className="ml-[32px]"
+          fansid={currentUid}
+          tgid={item.tg_id}
+          avatar={item.avatar}
+          username={item.tgname}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col h-full w-full text-white px-4 py-[30px]">
+      <h1 className="text-[24px] font-bold">{title}</h1>
+      <InfiniteScroll
+        dataLength={type === 'follower' ? follower.list.length : following.list.length}
+        next={() => {}}
+        hasMore={false}
+        loader={
+          <div className="flex items-center justify-center">
+            <Spinner color="#4A3AFF" />
+          </div>
+        }
+      >
+        {type === 'follower'
+          ? follower.list.map((item) => <FollowItem key={item.tg_id} item={item} />)
+          : following.list.map((item) => <FollowItem key={item.tg_id} item={item} />)}
+      </InfiniteScroll>
+    </div>
+  )
+}
+
+export default FollowPage

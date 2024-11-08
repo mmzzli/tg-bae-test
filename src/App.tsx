0ea05ae1d1f30e90d614bcdb65dfc82e14728d0f
gray <516465335@qq.com>
2024-11-08 10:31:23 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -15,31 +15,31 @@ import Profile from '@/pages/Profile'
 import ProfileEdit from '@/pages/Profile/edit'
 import OthersProfile from '@/pages/OthersProfile'
 import EarningsHistory from '@/components/PersonalDetails/Earnings/History'
-import { mockTelegramEnv, parseInitData } from '@tma.js/sdk'
-import { DEV_INIT_DATA_RAW } from './utils/constants'
 import Follow from './pages/Follow'
+// import { mockTelegramEnv, parseInitData } from '@tma.js/sdk'
+// import { DEV_INIT_DATA_RAW } from './utils/constants'
 
-mockTelegramEnv({
-  themeParams: {
-    accentTextColor: '#6ab2f2',
-    bgColor: '#17212b',
-    buttonColor: '#5288c1',
-    buttonTextColor: '#ffffff',
-    destructiveTextColor: '#ec3942',
-    headerBgColor: '#17212b',
-    hintColor: '#708499',
-    linkColor: '#6ab3f3',
-    secondaryBgColor: '#232e3c',
-    sectionBgColor: '#17212b',
-    sectionHeaderTextColor: '#6ab3f3',
-    subtitleTextColor: '#708499',
-    textColor: '#f5f5f5',
-  },
-  initData: parseInitData(DEV_INIT_DATA_RAW),
-  initDataRaw: DEV_INIT_DATA_RAW,
-  version: '7.2',
-  platform: 'tdesktop',
-})
+// mockTelegramEnv({
+//   themeParams: {
+//     accentTextColor: '#6ab2f2',
+//     bgColor: '#17212b',
+//     buttonColor: '#5288c1',
+//     buttonTextColor: '#ffffff',
+//     destructiveTextColor: '#ec3942',
+//     headerBgColor: '#17212b',
+//     hintColor: '#708499',
+//     linkColor: '#6ab3f3',
+//     secondaryBgColor: '#232e3c',
+//     sectionBgColor: '#17212b',
+//     sectionHeaderTextColor: '#6ab3f3',
+//     subtitleTextColor: '#708499',
+//     textColor: '#f5f5f5',
+//   },
+//   initData: parseInitData(DEV_INIT_DATA_RAW),
+//   initDataRaw: DEV_INIT_DATA_RAW,
+//   version: '7.2',
+//   platform: 'tdesktop',
+// })
 
 function App() {
   return (

078588adb95fdb2c8fb04c949fbc0051b6cf686a
talk0x <talk@dn2.life>
2024-11-12 16:12:49 +0800

    update

--- src/components/PersonalDetails/UserProfile.tsx
+++ src/components/PersonalDetails/UserProfile.tsx
@@ -1,26 +1,26 @@
-import { FC } from 'react'
-import { Heading, HStack, Box, Text, Link } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom'
-import { useStore } from '@/store'
-import Image from '../Image/Image'
-import EarningsPage from '@/components/PersonalDetails/Earnings'
-import ShareUser from './ShareUser'
-import { EditIcon } from '@/assets/icons'
-import { useTMAUtils } from '@/hooks/useTMAUtils'
-
-import { profileImg } from '@/assets/image'
+import { FC, useState } from 'react';
+import { Heading, HStack, Box, Text, Link } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { useStore } from '@/store';
+import Image from '../Image/Image';
+import EarningsPage from '@/components/PersonalDetails/Earnings';
+import ShareUser from './ShareUser';
+import { EditIcon } from '@/assets/icons';
+import { useTMAUtils } from '@/hooks/useTMAUtils';
+import { profileImg } from '@/assets/image';
 
 const UserProfile: FC = () => {
-  const { launchParams } = useTMAUtils()
-  const uid = launchParams.initData?.user?.id ?? 0
-  const userInfo = useStore((state) => state.userInfo)
-  const navigate = useNavigate()
+  const { launchParams } = useTMAUtils();
+  const userId = launchParams.initData?.user?.id ?? 0;
+  const userInfo = useStore((state) => state.userInfo);
+  const navigate = useNavigate();
+  const [isBioExpanded, setIsBioExpanded] = useState(false);
 
-  console.log('userInfo', userInfo)
+  const toggleBioExpand = () => setIsBioExpanded((prev) => !prev);
 
   return (
-    <Box p="0px 16px" pt="30px">
-      <HStack gap="16px" pl="8px" justifyContent="space-between">
+    <Box padding="0 16px" paddingTop="30px">
+      <HStack gap="16px" paddingLeft="8px" justifyContent="space-between">
         <Image
           rect
           width={64}
@@ -29,56 +29,62 @@ const UserProfile: FC = () => {
           src={userInfo.avatar || profileImg}
           alt={userInfo.username}
         />
-        <div className="flex items-center">
+        <Box display="flex" alignItems="center">
           <EarningsPage />
           <ShareUser userInfo={userInfo} />
-        </div>
+        </Box>
       </HStack>
-      <HStack className="mt-4">
-        <Heading as="h3" color="#E0E2F6" fontWeight="500">
+
+      <HStack marginTop="4">
+        <Heading as="h3" color="#E0E2F6" fontWeight="500" fontSize="20px">
           {userInfo.username}
         </Heading>
         <Link onClick={() => navigate('/profile/edit')}>
-          <Image src={EditIcon} />
+          <Image src={EditIcon} alt="Edit Profile" />
         </Link>
       </HStack>
-      <HStack pt="24px" gap="56px">
-        <Box>
+
+      <HStack paddingTop="24px" gap="56px">
+        <Box textAlign="center">
           <Heading
             fontSize="20px"
             color="#E0E2F6"
             lineHeight="24px"
-            onClick={() => navigate(`/follow/${uid}?type=follower`)}
+            cursor="pointer"
+            onClick={() => navigate(`/follow/${userId}?type=follower`)}
           >
             {userInfo.follower}
           </Heading>
           <Text color="#8A8C91" fontSize="12px" lineHeight="14px">
-            followers
+            Followers
           </Text>
         </Box>
-        <Box>
+        <Box textAlign="center">
           <Heading
             fontSize="20px"
             color="#E0E2F6"
             lineHeight="24px"
-            onClick={() => navigate(`/follow/${uid}?type=following`)}
+            cursor="pointer"
+            onClick={() => navigate(`/follow/${userId}?type=following`)}
           >
             {userInfo.fans}
           </Heading>
           <Text color="#8A8C91" fontSize="12px" lineHeight="14px">
-            following
+            Following
           </Text>
         </Box>
       </HStack>
-      <Box pt="16px" pb="24px" borderBottom="1px solid #212121">
-        <Text color="#62636F" fontSize="14px" lineHeight="16px" mb="4px">
-          {userInfo.bio}
+
+      <Box paddingTop="16px" paddingBottom="24px" borderBottom="1px solid #212121">
+        <Text color="#62636F" fontSize="14px" lineHeight="16px" marginBottom="4px">
+          {isBioExpanded ? userInfo.bio : `${userInfo.bio.slice(0, 100)}...`}
         </Text>
-        {/* <Link color="#4452FF" fontSize="14px">
-          More
-        </Link> */}
+        <Link color="#4452FF" fontSize="14px" onClick={toggleBioExpand}>
+          {isBioExpanded ? 'Less' : 'More'}
+        </Link>
       </Box>
     </Box>
-  )
-}
-export default UserProfile
+  );
+};
+
+export default UserProfile;

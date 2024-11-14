78a03cd7fc5abf4b89d394ebb6cf4b957d1de8ad
talk0x <talk@dn2.life>
2024-11-14 21:01:39 +0800

    fix

--- src/components/Image/VideoPreview.tsx
+++ src/components/Image/VideoPreview.tsx
@@ -1,33 +1,31 @@
-
 import React, { useEffect, useRef } from 'react';
 import Hls from 'hls.js';
-import { X } from 'lucide-react'
+import { X } from 'lucide-react';
 
 interface VideoPreviewProps {
-  isOpen: boolean
-  onClose: () => void
-  videoUrl: string
+  isOpen: boolean;
+  onClose: () => void;
+  videoUrl: string;
 }
 
 const VideoPreview: React.FC<VideoPreviewProps> = ({
   isOpen,
   onClose,
-  videoUrl
+  videoUrl,
 }) => {
-
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
     if (Hls.isSupported()) {
       const hls = new Hls();
-
       hls.loadSource(videoUrl);
-
       if (videoRef.current) {
         hls.attachMedia(videoRef.current);
       }
       hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
-        console.log('Manifest loaded:', data);
+        const maxLevel = hls.levels.length - 1;
+        hls.startLevel = maxLevel;
+        hls.currentLevel = maxLevel;
       });
       return () => {
         hls.destroy();
@@ -37,11 +35,17 @@ const VideoPreview: React.FC<VideoPreviewProps> = ({
     }
   }, [videoUrl]);
 
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = false;
+    }
+  }, [isOpen]);
+
   return (
     <div
       className="fixed inset-0 z-[9999] bg-black/90 backdrop-blur-sm"
       onClick={(e) => {
-        if (e.target === e.currentTarget) onClose()
+        if (e.target === e.currentTarget) onClose();
       }}
     >
       <button
@@ -51,21 +55,20 @@ const VideoPreview: React.FC<VideoPreviewProps> = ({
         <X className="w-6 h-6" />
       </button>
 
-      <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-[24px] w-[100%]'>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-[24px] w-[100%]">
         <video
           ref={videoRef}
           controls
           autoPlay
-          muted
           loop
           width="100%"
-          style={{ maxWidth: '100%', height: 'auto', borderRadius:'4px' }}
+          style={{ maxWidth: '100%', height: 'auto', borderRadius: '4px' }}
         >
           <p>Your browser does not support HTML5 video.</p>
         </video>
       </div>
-
     </div>
-  )
-}
-export default VideoPreview
+  );
+};
+
+export default VideoPreview;

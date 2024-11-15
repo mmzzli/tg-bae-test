31fce6694e4a833eb54e416a4b4f0c75bd2fb661
talk0x <talk@dn2.life>
2024-11-15 14:12:48 +0800

    update tg confirm

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -68,7 +68,19 @@ export const MainLayout: React.FC = () => {
       tgApp.BackButton.onClick(() => {
         console.log('location.pathname', location.pathname)
         if (location.pathname === '/home') {
-          tgApp.close()
+          tgApp.showConfirm({
+            text: 'Are you sure you want to continue?',
+            ok_button: 'Yes',
+            cancel_button: 'No',
+          }).then((result:boolean) => {
+            if (result) {
+              tgApp.close()
+            } else {
+              console.log(1)
+            }
+          }).catch((error:Error) => {
+            console.error("Error showing confirmation:", error);
+          });
         } else {
           window.history.back()
         }

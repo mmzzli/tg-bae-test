eb2aa65e8d43aa462fa913975989365f41f46c38
gray <516465335@qq.com>
2024-11-19 19:25:20 +0800

    fix(Remove Useless Code)

--- src/pages/OthersProfile/routeGuard.tsx
+++ src/pages/OthersProfile/routeGuard.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useLayoutEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useStore } from '@/store'
 import { DefaultAvatarIcon } from '@/assets/icons'
@@ -22,22 +22,22 @@ const ProfileGuard: FC<ProfileGuardProps> = ({ children }) => {
     myFollow: state.myFollow,
     setMyFollow: state.setMyFollow,
   }))
-  useLayoutEffect(() => {
+  useEffect(() => {
     const prepare = async () => {
       try {
         // Reset user info if viewing a different user's profile
-        if (othersUserInfo.uid !== -1 && othersUserInfo.uid !== Number(uid)) {
-          setOthersUserInfo({
-            uid: -1,
-            username: '-',
-            avatar: DefaultAvatarIcon,
-            bio: '',
-            fans: 0,
-            follower: 0,
-            user_id: -1,
-          })
-          resetOthersViewList()
-        }
+        // if (othersUserInfo.uid !== -1 && othersUserInfo.uid !== Number(uid)) {
+        //   setOthersUserInfo({
+        //     uid: -1,
+        //     username: '-',
+        //     avatar: DefaultAvatarIcon,
+        //     bio: '',
+        //     fans: 0,
+        //     follower: 0,
+        //     user_id: -1,
+        //   })
+        //   resetOthersViewList()
+        // }
 
         // Fetch user profile data if uid and token are available
         if (uid && token) {

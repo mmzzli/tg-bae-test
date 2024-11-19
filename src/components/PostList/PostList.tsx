c3faea45dbebeeafb0c2ea7c63bf3c77fb88074b
talk0x <talk@dn2.life>
2024-11-19 16:03:18 +0800

    fix empty

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -4,6 +4,9 @@ import ResourceList from '../ResourceList/ResourceList'
 import { cn } from '@/utils/utils'
 import { FormatterListItem } from '../../store/slices/resourceListSlice'
 
+import Empty from '../comm/Empty'
+import Icon from '../comm/Icon'
+
 interface PostListProps {
   className?: string
   list: FormatterListItem[]
@@ -12,25 +15,29 @@ interface PostListProps {
 }
 
 const PostList = ({ className, list, hasMore, fetchMoreData }: PostListProps) => {
-  return (
-    <div className={cn(className, 'pb-24')}>
-      <InfiniteScroll
-        dataLength={list.length}
-        next={fetchMoreData}
-        hasMore={hasMore}
-        loader={
-          <Box textAlign="center" m="20px 0">
-            <Spinner color="#4A3AFF" />
-          </Box>
-        }
-        scrollableTarget="ProfileScrollableDiv"
-        scrollThreshold={0.8}
-        style={{ overflow: 'visible' }}
-      >
-        <ResourceList resources={list} />
-      </InfiniteScroll>
-    </div>
-  )
+  if([].length){
+    return (
+      <div className={cn(className, 'pb-24')}>
+        <InfiniteScroll
+          dataLength={list.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={
+            <Box textAlign="center" m="20px 0">
+              <Spinner color="#4A3AFF" />
+            </Box>
+          }
+          scrollableTarget="ProfileScrollableDiv"
+          scrollThreshold={0.8}
+          style={{ overflow: 'visible' }}
+        >
+          <ResourceList resources={list} />
+        </InfiniteScroll>
+      </div>
+    )
+  }else{
+    return <Empty title="No post yet." icon={<Icon name="icon-none_post" style={{width:'164px', height:'164px'}}></Icon>}></Empty>
+  }
 }
 
 export default PostList

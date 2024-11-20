7105c8d9c0288f0630a64d6cd7589c85b6230012
talk0x <talk@dn2.life>
2024-11-20 17:57:43 +0800

    fix cache

--- src/components/ResourceList/ResourceList.tsx
+++ src/components/ResourceList/ResourceList.tsx
@@ -109,49 +109,49 @@ const ResourceList = ({ resources: initialResources }: { resources: FormatterLis
   }, [resources])
 
 
-  useEffect(() => {
-    const cacheVideos = async (resources:any) => {
-      const totalVideos = resources.length;
-      const progressArray = new Array(totalVideos).fill(0);
-      resources.map((item:any, index:number) =>{
-          if (Hls.isSupported()) {
-            const hls = new Hls();
-            const targetFragments = 1;
-            let bufferedFragments = 0;
+  // useEffect(() => {
+  //   const cacheVideos = async (resources:any) => {
+  //     const totalVideos = resources.length;
+  //     const progressArray = new Array(totalVideos).fill(0);
+  //     resources.map((item:any, index:number) =>{
+  //         if (Hls.isSupported()) {
+  //           const hls = new Hls();
+  //           const targetFragments = 1;
+  //           let bufferedFragments = 0;
 
-            hls.loadSource(item.media[0]);
-            hls.attachMedia(document.createElement("video"));
+  //           hls.loadSource(item.media[0]);
+  //           hls.attachMedia(document.createElement("video"));
 
-            hls.on(Hls.Events.FRAG_BUFFERED, () => {
-              bufferedFragments++;
-              progressArray[index] = bufferedFragments;
-              console.log(`视频 ${index + 1} 缓存分片数量: ${bufferedFragments}`);
-              if (bufferedFragments >= targetFragments) {
-                if (index + 1 === resources.length) {
-                  console.log('缓存完成')
-                }
-                hls.destroy();
-              }
-            });
-            hls.on(Hls.Events.MANIFEST_PARSED, () => {
-              console.log(`视频 ${index + 1} 流解析完成，开始缓存`);
-            });
+  //           hls.on(Hls.Events.FRAG_BUFFERED, () => {
+  //             bufferedFragments++;
+  //             progressArray[index] = bufferedFragments;
+  //             console.log(`视频 ${index + 1} 缓存分片数量: ${bufferedFragments}`);
+  //             if (bufferedFragments >= targetFragments) {
+  //               if (index + 1 === resources.length) {
+  //                 console.log('缓存完成')
+  //               }
+  //               hls.destroy();
+  //             }
+  //           });
+  //           hls.on(Hls.Events.MANIFEST_PARSED, () => {
+  //             console.log(`视频 ${index + 1} 流解析完成，开始缓存`);
+  //           });
 
-            hls.on(Hls.Events.ERROR, (event, data) => {
-              hls.destroy();
-            });
-          } else {
-            console.error("HLS.js 不支持当前浏览器环境");
-          }
-        }
-      );
-    };
+  //           hls.on(Hls.Events.ERROR, (event, data) => {
+  //             hls.destroy();
+  //           });
+  //         } else {
+  //           console.error("HLS.js 不支持当前浏览器环境");
+  //         }
+  //       }
+  //     );
+  //   };
 
-    if (initialResources && initialResources.length > 0) {
-      const resources = initialResources.filter(item => item.type === 0);
-      cacheVideos(resources);
-    }
-  }, [initialResources]);
+  //   if (initialResources && initialResources.length > 0) {
+  //     const resources = initialResources.filter(item => item.type === 0);
+  //     cacheVideos(resources);
+  //   }
+  // }, [initialResources]);
 
   const linkEve = async (post_id: number, boll: boolean) => {
     setLikes((prevLikes) =>

49c76bf650ce7370fbf32d8c7ab24725b8bcc784
gray <516465335@qq.com>
2024-11-21 11:34:23 +0800

    fix(BackButton)

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -79,10 +79,11 @@ export const MainLayout: React.FC = () => {
       })
       tgApp.BackButton.onClick(() => {
         console.log('location.pathname', location.pathname)
-        if (location.pathname === '/home') {
+        console.log('window.location.pathname', window.location.pathname)
+        if (window.location.pathname === '/home') {
           tgApp
             .showConfirm({
-              message: 'Are you sure you want to continue?',
+              message: 'Are you sure you want to Exit?',
               ok_button: 'Yes',
               cancel_button: 'No',
             })
@@ -105,6 +106,7 @@ export const MainLayout: React.FC = () => {
           //     console.log(1)
           //   }
           // });
+          // window.history.back()
         } else {
           window.history.back()
         }

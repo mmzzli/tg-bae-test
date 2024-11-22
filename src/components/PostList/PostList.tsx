de613146561f47e8b791443762a8d72eb7453446
gray <516465335@qq.com>
2024-11-22 09:57:55 +0800

    feat(Others Post Loading)

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -6,6 +6,7 @@ import { FormatterListItem } from '../../store/slices/resourceListSlice'
 
 import Empty from '../comm/Empty'
 import Icon from '../comm/Icon'
+import PostSkeleton from '../Skeketon/PostSkeleton'
 
 interface PostListProps {
   className?: string
@@ -15,34 +16,34 @@ interface PostListProps {
 }
 
 const PostList = ({ className, list, hasMore, fetchMoreData }: PostListProps) => {
-  if (list.length) {
-    return (
-      <div className={cn(className, 'pb-24')}>
-        <InfiniteScroll
-          dataLength={list.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={
-            <Box textAlign="center" m="20px 0">
-              <Spinner color="#4A3AFF" />
-            </Box>
-          }
-          scrollableTarget="profileScrollableDiv"
-          scrollThreshold={0.8}
-          style={{ overflow: 'visible' }}
-        >
-          <ResourceList resources={list} />
-        </InfiniteScroll>
-      </div>
-    )
-  } else {
-    return (
-      <Empty
-        title="No post yet."
-        icon={<Icon name="icon-none_post" style={{ width: '164px', height: '164px' }}></Icon>}
-      ></Empty>
-    )
-  }
+  // if (list.length) {
+  return (
+    <div className={cn(className, 'pb-24')}>
+      <InfiniteScroll
+        dataLength={list.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <Box textAlign="center" m="20px 0">
+            <PostSkeleton />
+          </Box>
+        }
+        scrollableTarget="profileScrollableDiv"
+        scrollThreshold={0.8}
+        style={{ overflow: 'visible' }}
+      >
+        <ResourceList resources={list} />
+      </InfiniteScroll>
+    </div>
+  )
+  // } else {
+  //   return (
+  //     <Empty
+  //       title="No post yet."
+  //       icon={<Icon name="icon-none_post" style={{ width: '164px', height: '164px' }}></Icon>}
+  //     ></Empty>
+  //   )
+  // }
 }
 
 export default PostList

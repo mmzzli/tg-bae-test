370e218042b9a538c36993423a2940c73c7116b9
talk0x <talk@dn2.life>
2024-11-25 16:31:55 +0800

    update gate

--- src/pages/AgeGate/index.tsx
+++ src/pages/AgeGate/index.tsx
@@ -1,13 +1,27 @@
+import { FC, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import BaseButton from '@/components/BaseButton/BaseButton'
 import Image from '@/components/Image/Image'
 import { GateImg } from '@/assets/image'
 
-const AgeGate = ()=>{
+const AgeGate = () => {
+  const navigate = useNavigate()
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("ageGate")) {
+      navigate('/home')
+    }
+  }, [])
   return (
     <div className="px-[20px] fixed w-screen h-screen bg-black z-10 overflow-auto scrollbar-hide">
 
-      <Image className='mx-[auto] mt-[94px] mb-[50px]' src={GateImg}/>
+      <Image className='mx-[auto] mt-[94px] mb-[50px]' src={GateImg} />
 
       <h3 className="text-[#FFF] text-[24px] text-center">
         Are you 18 years of age or older?
@@ -22,16 +36,23 @@ const AgeGate = ()=>{
         </p>
       </div>
       <div className="mt-[88px] text-center">
-        <p className="text-[#E0E2F6] text-[12px]">
-          I have read and accept the
-          <a className="underline ml-[4px]">Terms of Service</a>
-        </p>
+        <div className='flex justify-center gap-[4px]'>
+          <input type="checkbox" checked={isChecked} onChange={handleChange} />
+          <p className="text-[#E0E2F6] text-[12px]">
+            I have read and accept the
+            <a className="underline ml-[4px]">Terms of Service</a>
+          </p>
+        </div>
         <BaseButton
           text="I’m 18 or older"
           height="48px"
           width="290px"
           className='m-[auto] mt-[16px]'
           handler={() => {
+            if(isChecked){
+              localStorage.setItem("ageGate", "1")
+              navigate('/home')
+            }
           }}
         />
       </div>

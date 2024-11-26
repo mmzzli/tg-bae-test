81ca28aa192d6812c203439a66f3585cdc227994
gray <516465335@qq.com>
2024-11-26 22:17:03 +0800

    feat(Chat Hooks)

---
+++ src/types/global.d.ts
@@ -0,0 +1,6 @@
+export type AnyLiteral = Record<string, any>
+export type AnyClass = new (...args: any[]) => any
+export type AnyFunction = (...args: any[]) => any
+export type AnyToVoidFunction = (...args: any[]) => void
+export type BooleanToVoidFunction = (value: boolean) => void
+export type NoneToVoidFunction = () => void

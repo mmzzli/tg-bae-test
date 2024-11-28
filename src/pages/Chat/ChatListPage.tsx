e8e87e470544748fb6db4eb5238b9ae45f448583
gray <516465335@qq.com>
2024-11-28 23:21:46 +0800

    feat(Light Theme):chat list,IOS message window

--- src/pages/Chat/ChatListPage.tsx
+++ src/pages/Chat/ChatListPage.tsx
@@ -167,7 +167,11 @@ const ChatListPage: FC<{ className?: string }> = ({ className }) => {
   console.log('chatListPage render')
   return (
     <div
-      className={cn('bg-black min-h-screen overflow-auto', 'scrollbar-hide', className)}
+      className={cn(
+        'dark:bg-black bg-white min-h-screen overflow-auto',
+        'scrollbar-hide',
+        className
+      )}
       onClick={handleContainerClick}
       style={{
         paddingTop: `calc(${window.getComputedStyle(document.documentElement).getPropertyValue('--tg-safe-area-inset-top') ? 'var(--tg-safe-area-inset-top) + 54px' : '32px'})`,
--- src/pages/Chat/MessagePageIOS.tsx
+++ src/pages/Chat/MessagePageIOS.tsx
@@ -149,7 +149,7 @@ const MessagePageIOS = () => {
   return (
     <div
       ref={containerRef}
-      className="absolute top-0 left-0 right-0 flex flex-col bg-[#000000] z-[999] overflow-auto scrollbar-hide"
+      className="absolute top-0 left-0 right-0 flex flex-col dark:bg-[#000000] bg-white z-[999] overflow-auto scrollbar-hide"
       style={{
         WebkitOverflowScrolling: 'touch',
         transition: 'height 0.3s ease-in-out',
@@ -178,7 +178,7 @@ const MessagePageIOS = () => {
           alt="avatar"
           className="w-[32px] h-[32px] rounded-full"
         />
-        <span className="text-[#FFFFFF] text-lg ml-2">{chatPeople?.username}</span>
+        <span className="dark:text-white text-[#333] text-lg ml-2">{chatPeople?.username}</span>
       </div>
 
       <MemoizedMessageList
@@ -189,7 +189,7 @@ const MessagePageIOS = () => {
 
       {/* FAKE INPUT */}
       <div
-        className={`'flex h-[68px] absolute bottom-0 left-0 right-0 bg-[#000000] pr-[14px] pt-[8px] pl-[42px] ${
+        className={`'flex h-[68px] absolute border-t border-t-[#EBEBF4] dark:border-t-black bottom-0 left-0 right-0 dark:bg-black bg-white pr-[14px] pt-[8px] pl-[42px] ${
           showInput ? 'hidden' : 'block'
         }`}
       >
@@ -197,10 +197,10 @@ const MessagePageIOS = () => {
           onClick={() => {
             inputRef.current?.focus()
           }}
-          className="flex items-center flex-1 h-[36px] text-sm bg-black border-[1px]
-        border-[#4B4B4D] rounded-full px-3"
+          className="flex items-center flex-1 h-[36px] text-sm dark:bg-black bg-[#F5F3F3] border-[1px]
+        dark:border-[#4B4B4D] border-[#F5F3F3] rounded-full px-3"
           style={{
-            color: message ? '#FFFFFF' : '#5D5D60',
+            color: message ? '#333333' : '#999999',
           }}
         >
           {message ? message : 'Type a Message....'}
@@ -228,7 +228,7 @@ const MessagePageIOS = () => {
       {/* REAL INPUT */}
       <div
         className={cn(
-          'flex h-[68px] absolute left-0 right-0 bg-[#000000] overflow-hidden',
+          'flex h-[68px] absolute left-0 right-0 dark:bg-black bg-[#ffffff] border-t dark:border-none border-t-[#F5F3F3] overflow-hidden',
           // isFocused ? 'opacity-100' : 'opacity-0',
           showInput ? 'bottom-0 opacity-100' : '-top-32 opacity-0'
         )}
@@ -239,9 +239,9 @@ const MessagePageIOS = () => {
           onChange={(e) => setMessage(e.target.value)}
           onKeyDown={handleKeyPress}
           type="text"
-          className="absolute left-[42px] right-4 top-[8px] h-[36px] text-sm bg-black border-[1px]
-           border-[#4B4B4D] focus:border-[#4B4B4D] rounded-full px-3 outline-none
-            text-white placeholder:text-[#5D5D60] pr-[60px]"
+          className="absolute left-[42px] right-4 top-[8px] h-[36px] text-sm border-[1px] dark:bg-black
+           dark:border-[#4B4B4D] dark:focus:border-[#4B4B4D] focus:border-[#F5F3F3] bg-[#F5F3F3] rounded-full px-3 outline-none
+            dark:text-white dark:placeholder:text-[#5D5D60] placeholder:text-[#999999] pr-[60px]"
           placeholder="Type a Message..."
         />
         <div

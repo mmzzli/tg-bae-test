bbb7c6e876cba6b335a2fd832b1a476571b1eed7
Jacob <mmzzline@gmail.com>
2024-11-29 08:26:58 +0800

    fix(video): video loaded

--- src/store/slices/resourceListSlice.ts
+++ src/store/slices/resourceListSlice.ts
@@ -39,8 +39,8 @@ export interface CacheVideo {
   id: string | number
   media: string
 }
-const recordsNum = 3
-const CACHE_VIDEOS_LIMIT = 3
+const recordsNum = 30
+const CACHE_VIDEOS_LIMIT = 29
 const MAX_FRAGMENTS = 1
 
 export interface ResourceListSlice {

37e0dd78cf86bbd46cb4cc79e6b1975f83ccbced
Jacob <mmzzline@gmail.com>
2024-12-02 19:18:16 +0800

    fix(bug): rm  api

--- src/components/PersonalDetails/ShareModal.tsx
+++ src/components/PersonalDetails/ShareModal.tsx
@@ -7,7 +7,8 @@ import { useTMAUtils } from '@/hooks/useTMAUtils'
 import { LinkIcon, TelegramIcon } from '@/assets/icons'
 import { useMemoizedFn, useRequest, useSetState } from 'ahooks'
 import useCopy from '@/hooks/useCopy'
-import { getLink, getShareInlineMessageId } from '@/api/list'
+// import { getLink, getShareInlineMessageId } from '@/api/list'
+import { getLink } from '@/api/list'
 
 interface ChildMethods {
   someMethod: (username: string, uid: number) => void

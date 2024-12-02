a305e636f3dac5f8bb5b1336ee496c83c74899f7
gray <516465335@qq.com>
2024-12-02 18:40:10 +0800

    feat(Share Profile)

--- src/components/PersonalDetails/ShareModal.tsx
+++ src/components/PersonalDetails/ShareModal.tsx
@@ -1,15 +1,13 @@
-import React, { forwardRef, useImperativeHandle } from 'react'
+import { forwardRef, useImperativeHandle } from 'react'
 import { Image, useBoolean } from '@chakra-ui/react'
 
 import { BaseModal } from '@/components/Modal/BaseModal'
 import BaseButton from '@/components/BaseButton/BaseButton'
 import { useTMAUtils } from '@/hooks/useTMAUtils'
-import { ShareIcon, LinkIcon, TelegramIcon } from '@/assets/icons'
+import { LinkIcon, TelegramIcon } from '@/assets/icons'
 import { useMemoizedFn, useRequest, useSetState } from 'ahooks'
-import { IUserInfo } from '@/types'
 import useCopy from '@/hooks/useCopy'
-import { getLink } from '@/api/list'
-import useMobile from '@/hooks/useMobile'
+import { getLink, getShareInlineMessageId } from '@/api/list'
 
 interface ChildMethods {
   someMethod: (username: string, uid: number) => void
@@ -19,12 +17,15 @@ const ShareModal = forwardRef<ChildMethods>(({}, ref) => {
   useImperativeHandle(ref, () => ({
     someMethod: (username: string, uid: number) => {
       getShareLink(username, uid)
+      setState({ uid })
       toggle()
     },
   }))
+  const [state, setState] = useSetState<Record<string, any>>({
+    hello: '',
+  })
   const { shareLink } = useTMAUtils()
   const [isBaseModalOpen, { toggle, off }] = useBoolean(false)
-  const isMobile = useMobile()
   const [links, setLinks] = useSetState<{ shareLink: string; copyLink: string }>({
     shareLink: '',
     copyLink: '',
@@ -37,6 +38,16 @@ const ShareModal = forwardRef<ChildMethods>(({}, ref) => {
     },
   })
 
+  const { runAsync: getInlineMessageId, loading: getInlineMessageIdLoading } = useRequest(
+    getShareInlineMessageId,
+    {
+      manual: true,
+      onSuccess(res) {
+        console.log(res)
+      },
+    }
+  )
+
   const getShareLink = useMemoizedFn(async (title: string, uid: number) => {
     const shareText = encodeURIComponent(title)
 
@@ -46,7 +57,87 @@ const ShareModal = forwardRef<ChildMethods>(({}, ref) => {
     console.log('copyLink', decodeURIComponent(copyLink))
 
     const shareLink = `https://t.me/share/url?url=${copyLink}&text=${shareText}`
+    // TODO 尝试使用新的api shareMessages
+    // savePreparedInlineMessage 使用InlineQueryResultPhoto数据格式 获取包装好的消息实体(PreparedInlineMessage) https://core.telegram.org/bots/api#preparedinlinemessage
+    // if (window.Telegram?.WebApp) {
+    //   console.warn('######## preparedInlineMessage start ########')
+    //   const WebApp = window.Telegram?.WebApp
+    //   try {
+    //     const response = await axios.post(
+    //       'https://api.telegram.org/bot7471427114:AAHQavHhTte8QIoo-0F8MpAkO6yaBW2TfeA/savePreparedInlineMessage',
+    //       JSON.stringify({
+    //         user_id: current_uid,
+    //         allow_user_chats: true,
+    //         allow_group_chats: true,
+    //         allow_bot_chats: true,
+
+    //         result: {
+    //           type: 'photo',
+    //           id: generateUUID(),
+    //           photo_url:
+    //             'https://customer-sn5y0tm58c41dbpc.cloudflarestream.com/29c86574a9f2c05b6df9841b5475a307/thumbnails/thumbnail.jpg?time=1s&height=1080',
+    //           thumbnail_url:
+    //             'https://customer-sn5y0tm58c41dbpc.cloudflarestream.com/29c86574a9f2c05b6df9841b5475a307/thumbnails/thumbnail.jpg?time=1s&height=400',
+    //           show_caption_above_media: true,
+    //           caption: `Bae If beauty were a crime, you’d be serving life`,
+    //           caption_entities: [
+    //             {
+    //               type: 'bold',
+    //               offset: 0,
+    //               length: 49,
+    //             },
+    //             {
+    //               type: 'expandable_blockquote',
+    //               offset: 4,
+    //               length: 45,
+    //             },
+    //           ],
+    //           reply_markup: {
+    //             inline_keyboard: [
+    //               [
+    //                 {
+    //                   text: 'Open the Share',
+    //                   url: `tg://resolve?domain=BaeDevBot&appname=BAE&startapp=ref=16775ac1e99f846e80de5a0218ada3cb13ea86936f4871853c80bdc8c997649f`,
+    //                 },
+    //               ],
+    //             ],
+    //           },
+    //         },
+    //       }),
+    //       {
+    //         headers: {
+    //           'Content-Type': 'application/json',
+    //         },
+    //       }
+    //     )
+    //     const preparedInlineMessage = response.data
+    //     console.warn(
+    //       '######## preparedInlineMessage result ########',
+    //       preparedInlineMessage.result.id
+    //     )
+    //     WebApp.shareMessage(preparedInlineMessage.result.id)
+    //   } catch (err) {
+    //     console.log(err)
+    //   }
+    // }
+    // 使用 shareMessages 发送包装好的消息的id
     setLinks({ shareLink, copyLink: decodeURIComponent(copyLink) })
+  })
+
+  const handShareWithTelegram = useMemoizedFn(async () => {
+    const { result } = await getInlineMessageId({ pid: state?.uid, uid: state?.uid })
+    console.log('result----->', result)
+    if (result.id) {
+      if (window.Telegram?.WebApp) {
+        const WebApp = window.Telegram?.WebApp
+        setTimeout(() => {
+          WebApp.shareMessage(result.id)
+        }, 0)
+      }
+      off()
+    } else {
+      console.warn('######## shareMessages Error ########', result)
+    }
   })
 
   return (
@@ -54,7 +145,7 @@ const ShareModal = forwardRef<ChildMethods>(({}, ref) => {
       <BaseModal
         isOpen={isBaseModalOpen}
         onClose={off}
-        height={isMobile ? '342px' : '300px'}
+        height="342px"
         animation={{
           duration: 400,
           timingFunction: 'ease-in-out',
@@ -70,21 +161,21 @@ const ShareModal = forwardRef<ChildMethods>(({}, ref) => {
         <div className="mt-4 w-full">
           <h3 className="font-bold text-2xl mb-[10px] text-[24px] text-[#333]">Share from Bae</h3>
           <div className="text-[15px] text-[#999]">Earn $Bae every time you share from Bae</div>
-          {isMobile && (
-            <div className="mt-12 mb-[18px] mx-4">
-              <BaseButton
-                text="Share via Telegram"
-                height="48px"
-                icon={<Image src={TelegramIcon} />}
-                handler={() => {
-                  shareLink(links.shareLink ?? '')
-                  off()
-                }}
-              />
-            </div>
-          )}
+          <div className="mt-12 mb-[18px] mx-4">
+            <BaseButton
+              text="Share via Telegram"
+              height="48px"
+              loading={getInlineMessageIdLoading}
+              icon={<Image src={TelegramIcon} />}
+              handler={() => {
+                // shareLink(links.shareLink ?? '')
+                handShareWithTelegram()
+                // off()
+              }}
+            />
+          </div>
 
-          <div className={isMobile ? 'mx-4' : 'mx-4 mt-[50px]'}>
+          <div className={'mx-4'}>
             <BaseButton
               text="Copy link"
               height="48px"

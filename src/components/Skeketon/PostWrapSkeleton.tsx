2e4911825bd39cc68d9935c3467afa5196c40b71
gray <516465335@qq.com>
2024-12-03 16:18:04 +0800

    feat(Skeleton Light Theme)

--- src/components/Skeketon/PostWrapSkeleton.tsx
+++ src/components/Skeketon/PostWrapSkeleton.tsx
@@ -1,16 +1,14 @@
 import PostSkeleton from './PostSkeleton'
-import { DrawSkeletonItem } from './ChatSkeleton'
 
 const PostWrapSkeleton = () => {
   return (
     <>
-      <div className="p-4  h-[56px] mt-[14px] absolute left-0 top-0 z-10  w-full h-full  flex-col bg-[#000000]">
+      <div className="p-4  h-[56px] mt-[14px] absolute left-0 top-0 z-10  w-full h-full  flex-col dark:bg-[#000000] bg-white">
         <PostSkeleton></PostSkeleton>
         <PostSkeleton></PostSkeleton>
       </div>
     </>
   )
 }
-
 
 export default PostWrapSkeleton

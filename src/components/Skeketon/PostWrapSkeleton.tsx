e7e24103bc293a9f15d545e784acafd731a98c3b
gray <516465335@qq.com>
2024-12-03 16:20:29 +0800

    fix(Classname conflict)

--- src/components/Skeketon/PostWrapSkeleton.tsx
+++ src/components/Skeketon/PostWrapSkeleton.tsx
@@ -3,7 +3,7 @@ import PostSkeleton from './PostSkeleton'
 const PostWrapSkeleton = () => {
   return (
     <>
-      <div className="p-4  h-[56px] mt-[14px] absolute left-0 top-0 z-10  w-full h-full  flex-col dark:bg-[#000000] bg-white">
+      <div className="p-4 mt-[14px] absolute left-0 top-0 z-10  w-full h-full flex-col dark:bg-[#000000] bg-white">
         <PostSkeleton></PostSkeleton>
         <PostSkeleton></PostSkeleton>
       </div>

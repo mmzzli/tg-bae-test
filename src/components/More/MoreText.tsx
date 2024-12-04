fe465149c944df2cd7346b6bbce5f12b23fc9b6a
Jacob <mmzzline@gmail.com>
2024-12-04 17:23:51 +0800

    fix: 🐛 moretext
    fix more text show

--- src/components/More/MoreText.tsx
+++ src/components/More/MoreText.tsx
@@ -13,6 +13,9 @@ const MoreText: React.FC<MoreTextProps> = ({ text, maxLines = 3 }) => {
   if (text.trim().length === 0) {
     return <></>
   }
+  if (text.trim().length < 150) {
+    return text
+  }
   return (
     <div
       style={{

09d20b573211bfbe6a41bfd032dd09312aa36a16
gray <516465335@qq.com>
2024-12-04 13:21:23 +0800

    feat(OthersProfile):loading icon

--- src/pages/OthersProfile/routeGuard.tsx
+++ src/pages/OthersProfile/routeGuard.tsx
@@ -66,7 +66,10 @@ const ProfileGuard: FC<ProfileGuardProps> = ({ children }) => {
   if (!ready) {
     return (
       <div className="fixed w-screen h-screen flex flex-col items-center justify-center bg-white dark:bg-[#0D0D0D] z-10">
-        <div className="w-10 h-10 border-4 border-t-4 border-t-[#D1D0DE] border-transparent rounded-full animate-spin"></div>
+        <i
+          className="iconfont icon-loading animate-spin text-[#6254FF]"
+          style={{ fontSize: '40px' }}
+        ></i>
       </div>
     )
   }

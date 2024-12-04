6fa18f6d62afd97780ec2335be0d7280a8019b5f
gray <516465335@qq.com>
2024-12-04 15:43:05 +0800

    feat(Message Padding Size)

--- src/components/Chat/MessageList.tsx
+++ src/components/Chat/MessageList.tsx
@@ -182,7 +182,7 @@ export const MessageList = ({ messages, className, channelInfo }: MessageListPro
         scrollableTarget="scrollableDiv"
         style={{ display: 'flex', flexDirection: 'column-reverse', overflow: 'visible' }} // start from bottom
       >
-        <div className="h-[38px]"></div>
+        <div className="h-[22px]"></div>
         {messageGroups.map((group) => (
           <div key={`group-${group.timestamp}`}>
             <TimeDevider timestamp={group.timestamp} />

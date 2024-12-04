d934b33676ff15705a4b945206e25c78ff0e0c3a
gray <516465335@qq.com>
2024-12-04 13:47:18 +0800

    fix(Loading):error loading icon position

--- src/pages/OthersProfile/routeGuard.tsx
+++ src/pages/OthersProfile/routeGuard.tsx
@@ -65,7 +65,7 @@ const ProfileGuard: FC<ProfileGuardProps> = ({ children }) => {
 
   if (!ready) {
     return (
-      <div className="fixed w-screen h-screen flex flex-col items-center justify-center bg-white dark:bg-[#0D0D0D] z-10">
+      <div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-white dark:bg-[#0D0D0D] z-10">
         <i
           className="iconfont icon-loading animate-spin text-[#6254FF]"
           style={{ fontSize: '40px' }}

dd934a014cc9f654a594a983857f943db72f1a44
gray <516465335@qq.com>
2024-12-09 19:07:37 +0800

    feat(Multi Entry)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 import mkcert from 'vite-plugin-mkcert'
 import { createFilter } from '@rollup/pluginutils'
-import * as fs from 'node:fs'
+import fs from 'fs'
+const htmlPlugin = (mode: string) => {
+  return {
+    name: 'html-transform',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/') {
+          req.url = `/${mode === 'production' ? 'index.prod.html' : 'index.dev.html'}`
+        }
+        next()
+      })
+    },
+  }
+}
 
 const cacheBusterPlugin = () => {
   const filter = createFilter(['**/*.tsx', '**/*.ts', '**/*.js', '**/*.jsx'])
@@ -29,17 +42,38 @@ const cacheBusterPlugin = () => {
 }
 
 export default defineConfig(({ mode }) => {
+  const htmlEntrypoint = mode === 'production' ? 'index.prod.html' : 'index.dev.html'
+
   return {
-    plugins: [react(), nodePolyfills(), mkcert(), cacheBusterPlugin()],
+    plugins: [
+      react(),
+      {
+        name: 'rename-output-html',
+        closeBundle() {
+          const distDir = path.resolve(__dirname, 'dist')
+          if (fs.existsSync(path.join(distDir, htmlEntrypoint))) {
+            fs.renameSync(path.join(distDir, htmlEntrypoint), path.join(distDir, 'index.html'))
+          }
+        },
+      },
+      htmlPlugin(mode),
+      nodePolyfills(),
+      mkcert(),
+      cacheBusterPlugin(),
+    ],
     build: {
       chunkSizeWarningLimit: 1000,
       rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, htmlEntrypoint),
+        },
         output: {
           manualChunks: {
             'react-vendor': ['react', 'react-dom', 'react-router-dom'],
             chakra: ['@chakra-ui/react', '@emotion/react', '@emotion/styled'],
             telegram: ['@tma.js/sdk'],
           },
+          app: 'index.html',
         },
       },
     },
@@ -47,6 +81,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       host: '127.0.0.1',
       https: false,
+      open: '/',
       // https: (() => {
       //   if (process.env.HTTPS_CERT_PEM && process.env.HTTPS_CERT_KEY) {
       //     return {

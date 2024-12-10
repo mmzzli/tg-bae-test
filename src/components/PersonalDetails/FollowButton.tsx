d0f50664cf24552f07030d5db6feef2d7413532b
talk0x <talk@dn2.life>
2024-12-10 15:38:41 +0800

    update BaseButton type

--- src/components/PersonalDetails/FollowButton.tsx
+++ src/components/PersonalDetails/FollowButton.tsx
@@ -61,7 +61,8 @@ const FollowButton: FC<{
       loading={followLoading}
       width="104px"
       handler={doFollow}
-      className={`bg-transparent border text-[#333333] border-[#333] ${className}`}
+      className={`bg-transparent border text-[#333333] border-[#CDCDD4] ${className}`}
+      loadingColor="#999"
       // loadingClassName="bg-transparent border text-[#333333] border-[#333]"
     />
   ) : (
--- src/components/SecondaryMenu/SecondaryMenu.tsx
+++ src/components/SecondaryMenu/SecondaryMenu.tsx
@@ -100,6 +100,7 @@ const SecondaryMenu = ({ mediaData, currentUid, className, type }: Props) => {
                   : `Follow`
               }
               loading={isFollowLoading}
+              loadingColor="#999"
               width={
                 followResource?.some((user) => user.uid === mediaData.uid && user.boll)
                   ? '104px'

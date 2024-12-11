5d5b6903df044919a4dcba665df93fe201fccc38
gray <516465335@qq.com>
2024-12-11 16:39:00 +0800

    feat(Post Title Margin)

--- src/components/ResourceList/ResourceList.tsx
+++ src/components/ResourceList/ResourceList.tsx
@@ -312,18 +312,23 @@ const ResourceList = ({
                 type={type}
               />
               <Box position="relative">
-                {data.act_type === 1 && <Box
-                  position="absolute"
-                  bottom="0px"
-                  w="100%"
-                  zIndex={11}
-                  onClick={()=> navigate('/home/christmas')}
-                >
-                  <HStack p="3px 16px" justifyContent="space-between" bg="rgba(0, 0, 0, 0.5)">
-                    <Text fontSize={14} color="#fff"> Explore more</Text>
-                    <i className="iconfont icon-icon_arrow_right text-[#fff] text-[20px]"></i>
-                  </HStack>
-                </Box>}
+                {data.act_type === 1 && (
+                  <Box
+                    position="absolute"
+                    bottom="0px"
+                    w="100%"
+                    zIndex={11}
+                    onClick={() => navigate('/home/christmas')}
+                  >
+                    <HStack p="3px 16px" justifyContent="space-between" bg="rgba(0, 0, 0, 0.5)">
+                      <Text fontSize={14} color="#fff">
+                        {' '}
+                        Explore more
+                      </Text>
+                      <i className="iconfont icon-icon_arrow_right text-[#fff] text-[20px]"></i>
+                    </HStack>
+                  </Box>
+                )}
                 {data.type === POST_TYPE_IMAGE ? (
                   <ImageCard
                     data={data}
@@ -439,11 +444,10 @@ const ResourceFooter = memo<ResourceFooterProps>(({ data, linkEve, onShare, save
   }, [saveds])
   return (
     <>
-      <div className="px-4 flex items-center justify-between h-6 py-3 box-content items-center">
-        <Flex gap="16px" alignItems="center">
-          <Flex
-            as={'button'}
-            alignItems={'center'}
+      <div className="px-4 flex items-center justify-between h-6 mt-3 box-content">
+        <div className="flex items-center gap-4">
+          <div
+            className="flex items-center"
             onClick={() => {
               linkEve(data)
             }}
@@ -460,9 +464,9 @@ const ResourceFooter = memo<ResourceFooterProps>(({ data, linkEve, onShare, save
               <i className="iconfont icon-like text-[#0D0D0D]" style={{ fontSize: '22px' }}></i>
             )}
             <span className="pl-1 text-sm text-[##0D0D0D]">{likeNum}</span>
-          </Flex>
-          <Box
-            className="w-6 h-6 flex items-center justify-center"
+          </div>
+          <div
+            className="w-6 h-6 flex items-center justify-center mt-[2px]"
             onClick={() => {
               savedEve(data)
             }}
@@ -475,8 +479,8 @@ const ResourceFooter = memo<ResourceFooterProps>(({ data, linkEve, onShare, save
                 style={{ fontSize: '22px' }}
               ></i>
             )}
-          </Box>
-        </Flex>
+          </div>
+        </div>
 
         <IconButton
           onClick={onShare}
@@ -493,19 +497,19 @@ const ResourceFooter = memo<ResourceFooterProps>(({ data, linkEve, onShare, save
 
       {(data.title || data.is_pay) && (
         <div className="px-4 py-3">
-          <p className="text-[#0F1419] dark:text-[#ccc] text-sm leading-6">
+          <p className="text-[#0F1419] dark:text-[#ccc] font-normal text-sm leading-4">
             <MoreText text={data.title} />
           </p>
-          <HStack pt="2" justifyContent="space-between">
+          <div className="flex items-center justify-between">
             {data.is_pay && (
-              <HStack gap="4px">
+              <div className="flex items-center gap-2 mt-1">
                 <p className="text-[#666666] dark:text-[#424048] text-[12px]">
                   Purchased for {data.price}
                 </p>
-                <Image src={StarsIcon} />
-              </HStack>
+                <Image src={StarsIcon} className="mb-1" />
+              </div>
             )}
-          </HStack>
+          </div>
         </div>
       )}
     </>

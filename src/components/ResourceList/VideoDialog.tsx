31e997d0aba317019f746170cc5b2591ce128863
Jacob <mmzzline@gmail.com>
2024-12-15 00:14:33 +0800

    feat: 🎸 add log

--- src/components/ResourceList/VideoDialog.tsx
+++ src/components/ResourceList/VideoDialog.tsx
@@ -197,10 +197,12 @@ const VideoDialog = () => {
       hls.attachMedia(video)
 
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video.play().catch((error) => {
-          console.log(error)
-          console.log('自动播放失败')
-        })
+        video.onloadeddata = () => {
+          video.play().catch((error) => {
+            console.log(error, 'error====jacob')
+            console.log('自动播放失败')
+          })
+        }
       })
 
       hls.on(Hls.Events.ERROR, () => {

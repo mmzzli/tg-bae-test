aafb6d9b26d3d0c60cf9e55d7796128583e648b9
gray <516465335@qq.com>
2024-12-16 10:59:05 +0800

    fix(Progress Bar Index)

--- src/components/ResourceList/VideoDialog.tsx
+++ src/components/ResourceList/VideoDialog.tsx
@@ -122,7 +122,7 @@ const UserInfo = memo(
             )}
           </div>
         </div>
-        <MoreText moreColor={'#fff'} bgColor={"#56554e"} moreLine={true} text={content || ''} />
+        <MoreText moreColor={'#fff'} bgColor={'#56554e'} moreLine={true} text={content || ''} />
       </div>
     )
   }
@@ -265,7 +265,7 @@ const VideoDialog = () => {
   return (
     <div
       style={{ display: url ? 'block' : 'none' }}
-      className="absolute w-screen h-screen bg-black  z-[999]"
+      className="absolute w-screen h-screen bg-black  z-[1000]"
     >
       <div className="fixed w-full h-full object-contain z-10 bg-black inset-0">
         <div ref={videoContainerRef} style={{ height: '89vh' }} className="relative bg-white ">

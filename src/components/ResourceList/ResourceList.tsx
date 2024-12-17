e6a0d4cfe8f42da852da569fe9d5d784ed25bb78
talk0x <talk@dn2.life>
2024-12-17 16:23:15 +0800

    fix

--- src/components/ResourceList/ResourceList.tsx
+++ src/components/ResourceList/ResourceList.tsx
@@ -78,7 +78,7 @@ export const ShareModal: React.FC<ShareModalProps> = ({
         if (window.Telegram?.WebApp) {
           const WebApp = window.Telegram?.WebApp
           setTimeout(() => {
-            WebApp.shareMessage(result.id,(e)=>{
+            WebApp.shareMessage(result.id,(e:any)=>{
               console.log(e)
             })
           }, 100)

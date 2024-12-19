918a27ddea7e36c037609ce256b5fe3812969c1c
talk0x <talk@dn2.life>
2024-12-19 15:59:35 +0800

    update z-index

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -176,7 +176,7 @@ const HomePage: FC = () => {
           </div>
         </div>
 
-        <div className="ml-auto flex gap-[13px] z-[1111] relative">
+        <div className="ml-auto flex gap-[13px] z-[111] relative">
           <div
             className="w-[48px] h-[48px] p-[12px] bg-[#F5F3F3] rounded-[50px] flex items-center justify-center cursor-pointer"
             onClick={() => navigate('/home/searching')}

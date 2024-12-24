4e710bf90985180955b6e577c722a986456be28f
gray <516465335@qq.com>
2024-12-24 21:43:26 +0800

    fix(Remove Props)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -217,7 +217,7 @@ const HomePage: FC = () => {
           <div
             className={`${userInfo.user_id !== -1 && userInfo.fans === 0 ? '' : ''}  relative ${videoOpen ? 'z-[112]' : ''}`}
           >
-            <RecommendList ref={childRef} containerRef={containerRef} />
+            <RecommendList ref={childRef} />
           </div>
           {hasMore && (
             <div className="mt-12">

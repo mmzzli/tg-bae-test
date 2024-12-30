fc4995cd30cc1bfc2398d1d5f309bb1b47d83637
talk0x <talk@dn2.life>
2024-12-30 20:00:51 +0800

    fix

--- src/components/NewPost/Stars.tsx
+++ src/components/NewPost/Stars.tsx
@@ -64,17 +64,17 @@ const Stars: FC<StarsProps> = ({ price, setPrice }) => {
     }
   }, [isOpen])
 
-  // useEffect(() => {
-  //   const handleKeyboardHide = () => {
-  //     setIsFocused(false)
-  //     window.scrollTo(0, 0);
-  //   };
-  //   window.addEventListener('focusout', handleKeyboardHide);
+  useEffect(() => {
+    const handleKeyboardHide = () => {
+      setIsFocused(false)
+      window.scrollTo(0, 0);
+    };
+    window.addEventListener('focusout', handleKeyboardHide);
 
-  //   return () => {
-  //     window.removeEventListener('focusout', handleKeyboardHide);
-  //   };
-  // }, []);
+    return () => {
+      window.removeEventListener('focusout', handleKeyboardHide);
+    };
+  }, []);
 
 
   return (

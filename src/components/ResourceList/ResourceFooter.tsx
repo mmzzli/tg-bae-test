c7e9ca989cc3e2b5675badffea2b437ea48d1581
talk0x <talk@dn2.life>
2024-12-30 10:09:47 +0800

    fix fav

--- src/components/ResourceList/ResourceFooter.tsx
+++ src/components/ResourceList/ResourceFooter.tsx
@@ -1,14 +1,10 @@
 import { Box, Flex, IconButton, useBoolean } from '@chakra-ui/react'
-import Lottie from 'lottie-react'
-import likeAnimationData from '@/assets/animations/like.json'
 import React, { useMemo, useState } from 'react'
 import { FormatterListItem } from '@/store/slices/resourceListSlice'
 import { useMemoizedFn, useRequest, useSetState } from 'ahooks'
 import { genShareLinkFn } from '@/utils/utils'
 import { getLink, getShareInlineMessageId } from '@/api/list'
 import { ShareModal } from '@/components/ResourceList/ResourceList'
-import { useStore } from '@/store'
-import { favDel, favPost, postLike } from '@/api'
 
 import Links from '@/components/ResourceList/Links'
 import Saved from '@/components/ResourceList/Saved'
@@ -41,80 +37,12 @@ const ResourceFooter: React.FC<ResourceFooterProps> = ({ data }) => {
     setLinks({ shareLink, copyLink: decodeURIComponent(copyLink) })
   })
 
-  const likes = useStore((state) => state.like)
-  const saveds = useStore((state) => state.save)
-
-  const liked = useMemo(() => {
-    return likes.find((like) => like.id === data.id)?.liked || false
-  }, [likes])
-
-  const likeNum = useMemo(() => {
-    return likes.find((like) => like.id === data.id)?.like || 0
-  }, [likes])
-
-  const saved = useMemo(() => {
-    return saveds.find((saved) => saved.id === data.id)?.saveds
-  }, [saveds])
-
-  const setLikes = useStore((state) => state.setPatchLike)
-  const setSaveds = useStore((state) => state.setPatchSave)
-
-  const linkEve = async (data: FormatterListItem) => {
-    const curLiked = likes.find((item) => item.id === data.id)?.liked
-    await postLike({
-      act_type: !curLiked ? 1 : 2,
-      post_id: data.id,
-    })
-    setLikes(data)
-  }
-
-  const savedEve = async (data: FormatterListItem) => {
-    const isSaved = saveds.find((item) => item.id === data.id)?.saveds
-    if (!isSaved) {
-      await favPost(data.id)
-    } else {
-      await favDel(data.id)
-    }
-    setSaveds(data)
-  }
 
   return (
     <div className="px-4 flex items-center justify-between box-border h-10">
       <Flex gap="4" alignItems="center">
         <Links data={data} className={`text-[#fff]`} iconClassName={`text-[#fff]`}/>
         <Saved data={data} iconClassName={`text-[#fff]`}/>
-        {/* <Flex
-          as={'button'}
-          alignItems={'center'}
-          onClick={() => {
-            linkEve(data)
-          }}
-        >
-          {liked ? (
-            <Lottie
-              animationData={likeAnimationData}
-              loop={false}
-              style={{
-                width: '24px',
-              }}
-            ></Lottie>
-          ) : (
-            <i className="iconfont icon-like text-white text-6" style={{ fontSize: '24px' }}></i>
-          )}
-          <span className="pl-1 text-sm text-white">{likeNum}</span>
-        </Flex>
-        <Box
-          className="w-6 h-6 flex items-center justify-center"
-          onClick={() => {
-            savedEve(data)
-          }}
-        >
-          {saved ? (
-            <i className="iconfont icon-saved text-[#FFCC5D]" style={{ fontSize: '24px' }}></i>
-          ) : (
-            <i className="iconfont icon-bookmark-line text-white" style={{ fontSize: '24px' }}></i>
-          )}
-        </Box> */}
       </Flex>
       <IconButton
         onClick={() => {

e46cd892a23730a5b1d285a6ca24b7b7dd134921
mmzzli <mmzzline@gmail.com>
2024-12-31 11:27:12 +0800

    fix: no pay user didn't show purchased word

--- src/components/ResourceList/HomeResourceList.tsx
+++ src/components/ResourceList/HomeResourceList.tsx
@@ -555,7 +555,7 @@ const ResourceFooter = memo<ResourceFooterProps>(({ data, onShare, type, setReso
             <MoreText text={data.title} bgColor={'#fff'} textColor={'#0F1419'} />
           </div>
           <div className="flex items-center justify-between">
-            {(((type === 'view' || type === 'recommend') && data.price > 0) || data.is_pay) && (
+            {(((type === 'view' || type === 'recommend') && data.price > 0) && data.is_pay) && (
               <div className="flex items-center gap-2 mt-1">
                 <p className="text-[#666666] dark:text-[#424048] text-[12px]">
                   Purchased for {data.price}

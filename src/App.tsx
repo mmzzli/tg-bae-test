feec9d5c215741391482b796e6390b41f87f6b3b
gray <516465335@qq.com>
2025-01-06 13:26:01 +0800

    feat(Add Dependence):wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { WagmiProvider } from 'wagmi'
+import { config } from '@/config/wagmi-config'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import theme from '@/theme'
 
@@ -23,14 +26,23 @@ import MessagePageRouteGuard from './pages/Chat/MessagePageRouteGuard'
 import AgeGate from '@/pages/AgeGate'
 import { lazy, Suspense, useEffect } from 'react'
 import NiceModal from '@ebay/nice-modal-react'
-
+import { TomoWalletTgSdkV2, TomoProvider } from '@tomo-inc/tomo-telegram-sdk'
+import PriceService from '@/utils/wallet/PriceService'
 import { mockTelegramEnv, parseInitData } from '@tma.js/sdk'
 import { DEV_INIT_DATA_RAW } from './utils/constants'
 import { AliveScope, KeepAlive } from 'react-activation'
+import '@tomo-inc/tomo-telegram-sdk/dist/styles.css'
+
+import { TOMO_META_DATA } from './config/tomo-config'
 
 const Task = lazy(() => import('./pages/Task'))
 
-if(import.meta.env.MODE === 'dev'){
+PriceService.init()
+
+new TomoWalletTgSdkV2({ injected: true, metaData: TOMO_META_DATA.metaData })
+const queryClient = new QueryClient()
+
+if (import.meta.env.MODE === 'dev') {
   mockTelegramEnv({
     themeParams: {
       accentTextColor: '#6ab2f2',
@@ -75,16 +87,18 @@ function App() {
     }
   }, [])
   return (
-    <>
-      <NiceModal.Provider>
-        <ChakraProvider resetCSS theme={theme}>
-          <AliveScope>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<MainLayout />}>
-                  <Route index element={<Splash />} />
-                  <Route path="home" element={<div></div>}>
-                    {/* <Route
+    <TomoProvider theme="light" supportedProviders={['EVM']} tomoOptions={TOMO_META_DATA}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <NiceModal.Provider>
+            <ChakraProvider resetCSS theme={theme}>
+              <AliveScope>
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<MainLayout />}>
+                      <Route index element={<Splash />} />
+                      <Route path="home" element={<div></div>}>
+                        {/* <Route
                       index
                       element={
                         <KeepAlive name="home">
@@ -92,70 +106,72 @@ function App() {
                         </KeepAlive>
                       }
                     /> */}
-                  </Route>
+                      </Route>
 
-                  <Route path="searching" element={<Searching />} />
-                  <Route path="christmas" element={<Christmas />} />
+                      <Route path="searching" element={<Searching />} />
+                      <Route path="christmas" element={<Christmas />} />
 
-                  <Route path="post" element={<NewPost />} />
-                  <Route path="shares" element={<Shares />} />
+                      <Route path="post" element={<NewPost />} />
+                      <Route path="shares" element={<Shares />} />
 
-                  <Route
-                    path="task"
-                    element={
-                      <Suspense
-                        fallback={
-                          <div className="flex items-center justify-center h-full pb-10">
-                            <i
-                              className="iconfont icon-loading animate-spin text-[#6254FF]"
-                              style={{ fontSize: '40px' }}
-                            />
-                          </div>
+                      <Route
+                        path="task"
+                        element={
+                          <Suspense
+                            fallback={
+                              <div className="flex items-center justify-center h-full pb-10">
+                                <i
+                                  className="iconfont icon-loading animate-spin text-[#6254FF]"
+                                  style={{ fontSize: '40px' }}
+                                />
+                              </div>
+                            }
+                          >
+                            <Task />
+                          </Suspense>
                         }
-                      >
-                        <Task />
-                      </Suspense>
-                    }
-                  />
+                      />
 
-                  {/* Profile  */}
-                  <Route path="profile">
-                    <Route
-                      index
-                      element={
-                        <Suspense fallback={null}>
-                          <KeepAlive name="profile">
-                            <Profile />
-                          </KeepAlive>
-                        </Suspense>
-                      }
-                    />
-                    <Route path="edit" element={<ProfileEdit />} />
-                    <Route path="earningsHistory" element={<EarningsHistory />} />
-                    <Route
-                      path=":uid"
-                      element={
-                        <ProfileGuard>
-                          <OthersProfile />
-                        </ProfileGuard>
-                      }
-                    />
-                  </Route>
+                      {/* Profile  */}
+                      <Route path="profile">
+                        <Route
+                          index
+                          element={
+                            <Suspense fallback={null}>
+                              <KeepAlive name="profile">
+                                <Profile />
+                              </KeepAlive>
+                            </Suspense>
+                          }
+                        />
+                        <Route path="edit" element={<ProfileEdit />} />
+                        <Route path="earningsHistory" element={<EarningsHistory />} />
+                        <Route
+                          path=":uid"
+                          element={
+                            <ProfileGuard>
+                              <OthersProfile />
+                            </ProfileGuard>
+                          }
+                        />
+                      </Route>
 
-                  {/* Follow */}
-                  <Route path="follow/:uid" element={<Follow />} />
+                      {/* Follow */}
+                      <Route path="follow/:uid" element={<Follow />} />
 
-                  {/* Chat */}
-                  <Route path="chat" element={<></>} />
-                  <Route path="chat/:uid" element={<MessagePageRouteGuard />} />
-                  <Route path="ageGate" element={<AgeGate />} />
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </AliveScope>
-        </ChakraProvider>
-      </NiceModal.Provider>
-    </>
+                      {/* Chat */}
+                      <Route path="chat" element={<></>} />
+                      <Route path="chat/:uid" element={<MessagePageRouteGuard />} />
+                      <Route path="ageGate" element={<AgeGate />} />
+                    </Route>
+                  </Routes>
+                </BrowserRouter>
+              </AliveScope>
+            </ChakraProvider>
+          </NiceModal.Provider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </TomoProvider>
   )
 }
 

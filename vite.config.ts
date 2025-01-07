50ecff4547f8fab34d4528e00fd3aa207b6445b8
gray <516465335@qq.com>
2025-01-07 17:23:35 +0800

    fix(Update Typescript)

--- vite.config.ts
+++ vite.config.ts
@@ -48,6 +48,7 @@ export default defineConfig(({ mode }) => {
             ton: ['ton', 'ton-core', 'ton-crypto', 'tonweb'],
             swiper: ['swiper'],
             axios: ['axios'],
+            wukongimjssdk: ['wukongimjssdk'],
           },
         },
       },

cb891464e0570b163f40015ae3e746f579fc0443
gray <516465335@qq.com>
2025-01-08 19:24:40 +0800

    fix(remove log)

--- src/components/Chat/MessageRender.tsx
+++ src/components/Chat/MessageRender.tsx
@@ -269,8 +269,6 @@ export const MessageRender: React.FC<MessageRenderProps> = ({
     }
   }, [isUploading])
 
-  console.log('message', message)
-
   if (!message) return null
 
   if (isImageMessage(message)) {

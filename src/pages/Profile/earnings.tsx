cbbfd3e511f9d85c11019aae4e5440fcf695bf64
gray <516465335@qq.com>
2025-01-10 16:47:16 +0800

    feat(Gas Limit)

--- src/pages/Profile/earnings.tsx
+++ src/pages/Profile/earnings.tsx
@@ -2,7 +2,15 @@ import { useEffect, useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useBoolean, useToast } from '@chakra-ui/react'
 import { parseUnits } from 'viem'
-import { useAccount, useReadContract, useSwitchChain, useWriteContract, useEstimateFeesPerGas, useEstimateGas, useWaitForTransactionReceipt } from 'wagmi'
+import {
+  useAccount,
+  useReadContract,
+  useSwitchChain,
+  useWriteContract,
+  useEstimateFeesPerGas,
+  useEstimateGas,
+  useWaitForTransactionReceipt,
+} from 'wagmi'
 import { abi } from '@/config/abi'
 import { useTMAUtils } from '@/hooks/useTMAUtils'
 import { supportEVMTokenList } from '@/config/wagmi-config'
@@ -16,9 +24,8 @@ import { formatNumber } from '@/utils/utils'
 import { CustomToast, typeOptions } from '@/components/comm/Toast'
 import ConnectModal from '@/components/Wallet/ConnectModal'
 
-
 interface RewardItem {
-  amount: string | number;
+  amount: string | number
 }
 
 const Earnings = () => {
@@ -47,20 +54,21 @@ const Earnings = () => {
   const [gifts, setGifts] = useState(0)
 
   const chainId = import.meta.env.VITE_APP_ENV === 'production' ? 56 : 97
-  const contractAddress  = import.meta.env.VITE_APP_ENV === 'production' ? `0xF165cFb92441544cF9DEF72427028Db85b0aDEe2` : `0xF165cFb92441544cF9DEF72427028Db85b0aDEe2`
+  const contractAddress =
+    import.meta.env.VITE_APP_ENV === 'production'
+      ? `0xF165cFb92441544cF9DEF72427028Db85b0aDEe2`
+      : `0xF165cFb92441544cF9DEF72427028Db85b0aDEe2`
 
   const { data: rewardByUidList, refetch } = useReadContract({
     abi,
     address: contractAddress as `0x${string}`,
     functionName: 'getRewardByUid',
-    args:[BigInt(current_uid)]
+    args: [BigInt(current_uid)],
   })
 
   const getTokenInfoByChainId = (chainId: number) => {
-    return supportEVMTokenList.filter(token => token.chainId === chainId);
-  };
-
-
+    return supportEVMTokenList.filter((token) => token.chainId === chainId)
+  }
 
   const { isSuccess: isConfirmed, error: receiptError } = useWaitForTransactionReceipt({
     hash,
@@ -69,7 +77,7 @@ const Earnings = () => {
   const walletWithdraw = async () => {
     const tokenInfo = getTokenInfoByChainId(chainId)
     console.log(tokenInfo)
-    if(!(rewardByUidList && rewardByUidList.length)) return
+    if (!(rewardByUidList && rewardByUidList.length)) return
     const token = rewardByUidList[0].token
     const amount = rewardByUidList[0].amount
     switchChain(
@@ -79,11 +87,11 @@ const Earnings = () => {
       {
         onSuccess: async () => {
           setLiading(true)
-          const {signature, deadline} = await giftSign({
+          const { signature, deadline } = await giftSign({
             receiver: `${address}` as `0x${string}`,
-            token:  token as `0x${string}`,
-            chainid:  chainId,
-            amount:  Number(amount)
+            token: token as `0x${string}`,
+            chainid: chainId,
+            amount: Number(amount),
           })
           const args1 = [
             BigInt(current_uid),
@@ -91,35 +99,41 @@ const Earnings = () => {
             token as `0x${string}`,
             amount,
             BigInt(deadline),
-            signature as `0x${string}`
+            signature as `0x${string}`,
           ]
           console.log(args1)
+
+          const gasConfig =
+            import.meta.env.VITE_APP_ENV === 'production'
+              ? {}
+              : {
+                  gas: BigInt(Number(gasLimit) * 3),
+                  maxFeePerGas: feesPerGas?.maxFeePerGas,
+                  maxPriorityFeePerGas: feesPerGas?.maxPriorityFeePerGas,
+                }
+
           writeContract({
             address: contractAddress as `0x${string}`,
             abi,
             functionName: 'withdrawToken',
-            args:[
+            args: [
               BigInt(current_uid),
               `${address}` as `0x${string}`,
               token as `0x${string}`,
               amount,
               BigInt(deadline),
-              signature as `0x${string}`
+              signature as `0x${string}`,
             ],
-            gas: BigInt(Number(gasLimit) * 3),
-            maxFeePerGas: feesPerGas?.maxFeePerGas,
-            maxPriorityFeePerGas: feesPerGas?.maxPriorityFeePerGas,
+            ...gasConfig,
           })
-
         },
-        onError:(e)=>{
+        onError: (e) => {
           console.log(e)
           setLiading(false)
-        }
+        },
       }
     )
   }
-
 
   const handleAfterConnect = () => {
     status === 'connected' && address && myTokensModalRef.current?.someMethod()
@@ -143,14 +157,12 @@ const Earnings = () => {
     }
   }
 
-
-  const verifyWithdrawEve = async()=>{
-
+  const verifyWithdrawEve = async () => {
     await verifyWithdraw({
-      from: address  as `0x${string}`,
+      from: address as `0x${string}`,
       chain_id: chainId,
       amount: gifts,
-      hash: hash  as `0x${string}`
+      hash: hash as `0x${string}`,
     })
     load()
 
@@ -163,11 +175,11 @@ const Earnings = () => {
     setLiading(false)
   }
 
-  useEffect(()=>{
-    if(isConfirmed){
+  useEffect(() => {
+    if (isConfirmed) {
       verifyWithdrawEve()
     }
-  },[isConfirmed])
+  }, [isConfirmed])
 
   useEffect(() => {
     if (error || receiptError) {
@@ -183,7 +195,7 @@ const Earnings = () => {
   }, [error, receiptError])
 
   const load = async () => {
-    const {withdraw_gifts} = await getTotalGifts()
+    const { withdraw_gifts } = await getTotalGifts()
     setGifts(withdraw_gifts)
     const res = await totalAvailableInvoice()
     setData(res)
@@ -200,15 +212,16 @@ const Earnings = () => {
     >
       <h3 className="text-[#333] text-[20px]">Earnings</h3>
       <div className="text-center mt-12">
-        <h2 className="text-[#12122A] text-[40px]">${formatNumber(data.total * data.exchange_rate)}</h2>
+        <h2 className="text-[#12122A] text-[40px]">
+          ${formatNumber(data.total * data.exchange_rate)}
+        </h2>
         <p className="text-[#999] text-[14px] mt-2">Total earnings</p>
       </div>
       <div className="flex justify-between items-center mt-12">
         <h4 className="text-[#333] text-[16px]">Telegram stars</h4>
-        <div className='flex gap-2'
-          onClick={() =>
-            navigate(`/profile/earningsHistory?exchange_rate=${data.exchange_rate}`)
-          }
+        <div
+          className="flex gap-2"
+          onClick={() => navigate(`/profile/earningsHistory?exchange_rate=${data.exchange_rate}`)}
         >
           <p className="text-[#666] text-[14px]">History</p>
           <img className="mt-[2px]" src={RightIcon} />
@@ -221,23 +234,19 @@ const Earnings = () => {
             <h5 className="text-[#000] text-[22px] my-4">
               ${formatNumber(data.total * data.exchange_rate)}
             </h5>
-            <p className="text-[#888] text-[12px]">
-              {data.total}⭐️
-            </p>
+            <p className="text-[#888] text-[12px]">{data.total}⭐️</p>
           </li>
           <li>
             <p className="text-[#999] text-[12px]">Total stars earned</p>
             <h5 className="text-[#000] text-[22px] my-4">
               ${formatNumber(data.available * data.exchange_rate)}
             </h5>
-            <p className="text-[#888] text-[12px]">
-              {data.available}⭐️
-            </p>
+            <p className="text-[#888] text-[12px]">{data.available}⭐️</p>
           </li>
         </ul>
-        <div className='px-3'>
+        <div className="px-3">
           <BaseButton
-            className='mt-5'
+            className="mt-5"
             text="Withdraw"
             width="100%"
             height="40px"
@@ -253,30 +262,35 @@ const Earnings = () => {
           />
         </div>
       </div>
-      <div className='mt-7'>
+      <div className="mt-7">
         <div className="flex justify-between items-center">
           <h4 className="text-[#333] text-[16px]">Tomo wallet</h4>
-          <div className='flex gap-2'>
+          <div className="flex gap-2">
             <p className="text-[#666] text-[14px]">History</p>
             <img className="mt-[2px]" src={RightIcon} />
           </div>
         </div>
         <div className="bg-[#F7F9FC] px-5 py-5 mt-3 rounded-lg">
-          <p className='text-[#999] text-[12px]'>Cryptos received</p>
-          <h3 className='text-[#000] text-[22px] my-4'>${formatNumber(gifts)}</h3>
-          <p className='text-[#666] text-[12px]'>This shows the estimated total value of crypto you received from others, subject to market fluctuations.</p>
-          <div className='px-3'>
-            {status === 'disconnected' ? <BaseButton
-              className='mt-5'
-              text="Connect wallet"
-              width="100%"
-              height="40px"
-              handler={() => {
-                handleReward()
-              }}
-            /> :
+          <p className="text-[#999] text-[12px]">Cryptos received</p>
+          <h3 className="text-[#000] text-[22px] my-4">${formatNumber(gifts)}</h3>
+          <p className="text-[#666] text-[12px]">
+            This shows the estimated total value of crypto you received from others, subject to
+            market fluctuations.
+          </p>
+          <div className="px-3">
+            {status === 'disconnected' ? (
               <BaseButton
-                className='mt-5'
+                className="mt-5"
+                text="Connect wallet"
+                width="100%"
+                height="40px"
+                handler={() => {
+                  handleReward()
+                }}
+              />
+            ) : (
+              <BaseButton
+                className="mt-5"
                 text="Lauch wallet to withdraw"
                 width="100%"
                 height="40px"
@@ -291,7 +305,7 @@ const Earnings = () => {
                   walletWithdraw()
                 }}
               />
-            }
+            )}
           </div>
         </div>
       </div>

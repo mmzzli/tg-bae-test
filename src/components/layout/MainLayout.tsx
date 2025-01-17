3e13a9390dbc56e906eaf1af33fa3e02e3eea215
zhouxiaopeng <devz@gmail.com>
2025-01-17 18:41:40 +0800

    fix: change config

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -103,8 +103,8 @@ export const MainLayout: React.FC = () => {
     let userInfo
     try {
       const { initDataRaw } = retrieveLaunchParams()
-      // userInfo = initDataRaw
-      userInfo = DEV_INIT_DATA_RAW
+      userInfo = initDataRaw
+      // userInfo = DEV_INIT_DATA_RAW
       log('userInfo finally', userInfo)
     } catch (error) {
       userInfo = DEV_INIT_DATA_RAW

7b4ec9d13e089cbd306a15b86a7b3e80e2c69a31
mmzzli <mmzzline@gmail.com>
2025-01-20 16:33:34 +0800

    feat: task ui fix

--- src/components/BaseButton/BaseButton.tsx
+++ src/components/BaseButton/BaseButton.tsx
@@ -31,10 +31,10 @@ const BaseButton = ({
       <div
         className={cn(
           'relative no-tap flex items-center justify-center gap-2 bg-[#6254FF] dark:bg-[#4A3AFF] rounded-[42px] text-white dark:text-[#E0E2F6] text-sm font-medium cursor-pointer',
-          className,
           disabled
-            ? 'dark:bg-[#6a5cfc] bg-[#D1D0DE] cursor-not-allowed text-white border-[#D1D0DE]'
-            : ''
+          ? 'dark:bg-[#6a5cfc] bg-[#D1D0DE] cursor-not-allowed text-white border-[#D1D0DE]'
+          : '',
+          className,
         )}
         onClick={() => !loading && !disabled && handler()}
         style={{ width, height }}

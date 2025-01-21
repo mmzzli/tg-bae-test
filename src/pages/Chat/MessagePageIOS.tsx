c475f90800a11c8945277e74467390514a654aa5
gray <516465335@qq.com>
2025-01-21 11:44:23 +0800

    feat: use image and video preview icon

--- src/pages/Chat/MessagePageIOS.tsx
+++ src/pages/Chat/MessagePageIOS.tsx
@@ -15,6 +15,7 @@ import { useProfileNavigation } from '@/hooks/useProfileNavigation'
 import { useDailyTaskActions } from '@/hooks/useDailyTask'
 import { debounce } from '@/utils/chat/schedulers'
 import RewardButton from '@/components/Wallet/RewardButton'
+import { ImagePreviewIcon, VideoPreviewIcon } from '@/components/Chat/MessageRender'
 // const PAGE_SIZE = 20
 const isIOS = () => {
   return /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
@@ -310,7 +311,12 @@ const MessagePageIOS = () => {
         ></div>
 
         {/* Media Message Preview */}
-        {replyMessage?.messageType !== MessageType.TEXT && <div></div>}
+        {replyMessage?.messageType === MessageType.IMAGE && (
+          <ImagePreviewIcon url={replyMessage?.message} />
+        )}
+        {replyMessage?.messageType === MessageType.VIDEO && (
+          <VideoPreviewIcon url={replyMessage?.message} />
+        )}
 
         <div className="flex flex-col flex-1 overflow-hidden text-sm">
           {/* Reply To */}

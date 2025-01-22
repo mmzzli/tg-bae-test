a9b5c50c372611a79e2b7080a92f7b472f0588d9
mmzzli <mmzzline@gmail.com>
2025-01-23 00:26:57 +0800

    feat: video style

--- src/components/ResourceList/FrostedGlass.tsx
+++ src/components/ResourceList/FrostedGlass.tsx
@@ -1,8 +1,7 @@
 import React, { FC, useEffect, useState } from 'react'
-import { Box, Flex, Text, IconButton, useBoolean } from '@chakra-ui/react'
+import { Box, Flex, Text, IconButton, useBoolean, HStack } from '@chakra-ui/react'
 import { useTMAUtils } from '@/hooks/useTMAUtils'
 import { useGA4EventTrackingReporting } from '@/hooks/useGA4EventTrackingReporting'
-
 import BaseButton from '@/components/BaseButton/BaseButton'
 import Image from '@/components/Image/Image'
 import { useRequest, useSafeState } from 'ahooks'
@@ -25,18 +24,31 @@ const FrostedGlass: FC<FrostedGlassProps> = ({ price, post_id, resourcesEve, mas
   return (
     <>
       {!isPaid && (
-        <div className={'bg-[rgba(0,0,0,0.2)] absolute inset-0 z-[3]'} onClick={maskOnClick}>
+        // <div className={'bg-[rgba(0,0,0,0.2)] absolute inset-0 z-[3]'} onClick={maskOnClick}>
+        <HStack
+            position="absolute"
+            bottom="0"
+            left="50%"
+            // p="5px"
+            // gap="4px"
+            rounded="full"
+            zIndex={4}
+            bg="rgba(0, 0, 0, 0.40)"
+            cursor="pointer"
+          >
           <Box
             position="absolute"
             bottom="7"
             left="50%"
             transform=" translate(-50%, -50%)"
             w="220px"
-            zIndex={3}
+            zIndex={666}
           >
             <PurchaseButton price={price} post_id={post_id} resourcesEve={resourcesEve} setIsPaid={setIsPaid} />
           </Box>
-        </div>
+
+        </HStack>
+
       )}
     </>
   )

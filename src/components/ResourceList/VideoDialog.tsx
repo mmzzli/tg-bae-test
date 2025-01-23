a748cb295ecf7e7c3562ba31b575a69114195042
mmzzli <mmzzline@gmail.com>
2025-01-23 16:50:41 +0800

    fix: pay video

--- src/components/ResourceList/VideoDialog.tsx
+++ src/components/ResourceList/VideoDialog.tsx
@@ -72,42 +72,6 @@ const ProgressDisplay = memo(
   )
 )
 
-const resourcesEve = (post_id: number, url: string, is_pay?: boolean) => {
-  // const recommendList = useStore((state) => state.recommendList)
-  console.log('update0...')
-  const info = useStore((state) => state.videoResource)
-  console.log('update1...')
-  const setVideoResource = useStore((state) => state.setVideoResource)
-  // const setRecommendList = useStore((state) => state.setRecommendList)
-  console.log('update2...')
-  const options = is_pay ? { is_pay } : {}
-  const medias = url.split(',')
-  const picUrl = medias.find((item) => !item.endsWith('.m3u8'))
-  const media = medias.find((item) => item.endsWith('.m3u8'))
-  console.log('update3...')
-  if (media) {
-    setVideoResource({ ...info, media: [media], mediaCover: picUrl ?? '', ...options } as FormatterListItem)
-  }
-  // const updatedUsers = recommendList.list.map((item) => {
-  //   if (item.id === post_id) {
-  //     const options = is_pay ? { is_pay } : {}
-  //     if (item.act_type === 0) {
-  //       const medias = url.split(',')
-  //       const picUrl = medias.find((item) => !item.endsWith('.m3u8'))
-  //       const media = medias.find((item) => item.endsWith('.m3u8'))
-  //       // setCacheVideoIndex(item.id)
-  //       if (media) {
-  //         setVideoResource({ ...item, media: [media], mediaCover: picUrl ?? '', ...options })
-  //         return { ...item, media: [media], mediaCover: picUrl ?? '', ...options }
-  //       }
-  //     }
-  //     return { ...item, media: url.split(','), ...options }
-  //   }
-  //   return item
-  // })
-  // setRecommendList(updatedUsers)
-}
-
 export const UserInfo = memo(
   ({
     avatar,
@@ -158,6 +122,37 @@ export const UserInfo = memo(
       jumpToProfilePage({ uid } as UserItem)
       setVideoResource(null)
     }, [uid])
+
+    const resourcesEve = (post_id: number, url: string, is_pay?: boolean) => {
+      // const recommendList = useStore((state) => state.recommendList)
+      console.log('update2...')
+      const options = is_pay ? { is_pay } : {}
+      const medias = url.split(',')
+      const picUrl = medias.find((item) => !item.endsWith('.m3u8'))
+      const media = medias.find((item) => item.endsWith('.m3u8'))
+      console.log('update3...')
+      if (media) {
+        setVideoResource({ ...info, media: [media], mediaCover: picUrl ?? '', ...options } as FormatterListItem)
+      }
+      // const updatedUsers = recommendList.list.map((item) => {
+      //   if (item.id === post_id) {
+      //     const options = is_pay ? { is_pay } : {}
+      //     if (item.act_type === 0) {
+      //       const medias = url.split(',')
+      //       const picUrl = medias.find((item) => !item.endsWith('.m3u8'))
+      //       const media = medias.find((item) => item.endsWith('.m3u8'))
+      //       // setCacheVideoIndex(item.id)
+      //       if (media) {
+      //         setVideoResource({ ...item, media: [media], mediaCover: picUrl ?? '', ...options })
+      //         return { ...item, media: [media], mediaCover: picUrl ?? '', ...options }
+      //       }
+      //     }
+      //     return { ...item, media: url.split(','), ...options }
+      //   }
+      //   return item
+      // })
+      // setRecommendList(updatedUsers)
+    }
 
     return (
       <div

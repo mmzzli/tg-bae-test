166e00e6db39a9ea5f68eba80cf45d7d94b58270
zhouxiaopeng <devz@gmail.com>
2025-01-24 15:00:52 +0800

    fix: ui bug

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -107,7 +107,10 @@ const Profile: FC = () => {
       </h3>
       <div
         ref={titleRef}
-        className="content-area absolute top-[113px] left-0 w-full h-full  bg-white dark:bg-black"
+        className="content-area absolute left-0 w-full h-full  bg-white dark:bg-black"
+        style={{
+          top: 'calc(164px - var(--tg-safe-area-inset-top))',
+        }}
       >
         <UserProfile />
         <ViewList />

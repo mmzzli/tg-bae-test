a2d05ab46e1bb9de4768f301ed57495289c20111
mmzzli <mmzzline@gmail.com>
2025-01-24 19:12:34 +0800

    fix: test keyboard shake

--- src/components/Wallet/SendRewardPage.tsx
+++ src/components/Wallet/SendRewardPage.tsx
@@ -64,7 +64,7 @@ const SendRewardPage = () => {
   const price = PriceService.getInstance().getPrice(token)
   return (
     <div
-      className="fixed top-0 left-0 bottom-0 right-0 bg-white z-[9999] px-[20px]"
+      className="fixed top-0 left-0 bottom-0 right-0 bg-white z-[10000] px-[20px]"
       style={{
         paddingTop: 'calc(var(--tg-safe-area-inset-top) + var(--tg-content-safe-area-inset-top))',
         paddingBottom:

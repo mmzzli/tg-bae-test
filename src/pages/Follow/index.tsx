ea492514ce0c02d7d44f4cbcd9d9b8989c3ab197
talk0x <talk@dn2.life>
2025-02-07 13:12:21 +0800

    update feat

--- src/pages/Follow/index.tsx
+++ src/pages/Follow/index.tsx
@@ -101,6 +101,9 @@ const FollowPage: FC = () => {
           className="flex-1 ml-3 truncate overflow-hidden whitespace-nowrap text-[#333] dark:text-black text-base"
         >
           {item.tgname}
+          <p className='text-[#999] text-[12px] font-normal'>
+            {item.fans_num} followers
+          </p>
         </div>
         {item.tg_id !== currentUid && (
           <FollowButton

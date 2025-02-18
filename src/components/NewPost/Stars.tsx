e71da6761aa825bd737b1847f6f63230d68b8315
talk0x <talk@dn2.life>
2025-02-18 19:04:57 +0800

    fix

--- src/components/NewPost/Stars.tsx
+++ src/components/NewPost/Stars.tsx
@@ -86,20 +86,35 @@ const Stars: FC<StarsProps> = ({ price, setPrice, featureRefBoll }) => {
     };
   }, []);
 
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
   useEffect(() => {
     setHeights(window.innerHeight)
-    const handleResize = () => {
+    const updateKeyboardHeight = () => {
+      const fullHeight = window.innerHeight;
+      const availableHeight =
+        window.Telegram?.WebApp?.viewportStableHeight
+      console.log(fullHeight, 'ccc', availableHeight)
       if(heights > window.innerHeight){
-        setIsFocused(true)
+        setKeyboardHeight(availableHeight)
       }else{
-        setIsFocused(false)
+        setKeyboardHeight(0)
       }
+
     };
-    window.addEventListener('resize', handleResize);
+
+    // 监听 Telegram 视口变化
+    window.Telegram?.WebApp?.onEvent("viewportChanged", updateKeyboardHeight);
+
+    // 监听普通网页的 `visualViewport` 变化
+    window.visualViewport?.addEventListener("resize", updateKeyboardHeight);
+
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.Telegram?.WebApp?.offEvent("viewportChanged", updateKeyboardHeight);
+      window.visualViewport?.removeEventListener("resize", updateKeyboardHeight);
     };
   }, []);
+
 
 
   return (
@@ -193,8 +208,7 @@ const Stars: FC<StarsProps> = ({ price, setPrice, featureRefBoll }) => {
               />}
               <Image src={StarsIcon} alt="Stars Icon" />
             </HStack>
-
-            <Box p="0px 18px" h={`${isFocused ? "400px" : ""}`}>
+            <Box p="0px 18px" minH={`${keyboardHeight}px`}>
               {boll && <Button
                 size="xl"
                 fontSize="14px"

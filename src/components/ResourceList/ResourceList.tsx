c3cdd9a943a44ab06f9089b284367b05b0cc785e
mmzzli <mmzzline@gmail.com>
2025-02-21 15:45:31 +0800

    feat: event bubbing

--- src/components/ResourceList/ResourceList.tsx
+++ src/components/ResourceList/ResourceList.tsx
@@ -729,7 +729,10 @@ const ResourceFooter = memo<ResourceFooterProps>(({ data, linkEve, onShare, save
 
 export const PlayButton = memo(({ onClick }: { onClick: (e: React.MouseEvent) => void }) => (
   <div
-    onClick={onClick}
+    onClick={(e) => {
+      e.stopPropagation()
+      onClick(e)
+    }}
     className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 no-tap z-[5] video-card-switch"
   >
     {/* <Image src={playIcon} alt="play" className="w-[72px] h-[72px] no-tap" /> */}

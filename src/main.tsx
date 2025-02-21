b3089e272abf5ec904fc4804046f2023ecd6f745
mmzzli <mmzzline@gmail.com>
2025-02-21 15:04:04 +0800

    feat: auto player

--- src/main.tsx
+++ src/main.tsx
@@ -50,17 +50,17 @@ if (import.meta.env.MODE === 'production') {
 // window.addEventListener('resize', setRem);
 
 
-Sentry.init({
-  dsn: 'https://0201b69ad2e7cf4be3c444e2782ed79f@o4508635704590336.ingest.us.sentry.io/4508635707211776',
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  environment: import.meta.env.MODE,
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['*', '/api/*'],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-})
+// Sentry.init({
+//   dsn: 'https://0201b69ad2e7cf4be3c444e2782ed79f@o4508635704590336.ingest.us.sentry.io/4508635707211776',
+//   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+//   environment: import.meta.env.MODE,
+//   // Tracing
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+//   tracePropagationTargets: ['*', '/api/*'],
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// })
 
 createRoot(document.getElementById('root')!).render(<App />)

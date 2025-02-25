4c3298f1fcca15753c7d934cdb5a7f80253aa9d7
mmzzli <mmzzline@gmail.com>
2025-02-25 18:00:20 +0800

    feat: reset muted

--- src/pages/TTPlayer/index.tsx
+++ src/pages/TTPlayer/index.tsx
@@ -1,16 +1,27 @@
 'use client'
 
+import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import './index.css'
 import HomeList from '@/components/TT/HomeList'
 import ChristmasList from '@/components/TT/ChristmasList'
+import { destroyVideo } from '@/utils/utils'
+import { useStore } from '@/store'
 
 export interface TVideo {
   url: string
   poster?: string
 }
+
 const TTPlayer: React.FC = () => {
   const { state } = useLocation()
+  const setTtVideoMuted = useStore((state) => state.setTtVideoMuted)
+  useEffect(() => {
+    return () => {
+      setTtVideoMuted(true)
+      destroyVideo()
+    }
+  }, [])
   return (
       <>
         {state.sourcePath === '/home' ? <HomeList /> : <ChristmasList />}

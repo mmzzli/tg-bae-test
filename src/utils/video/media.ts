15d1c4ee6f99b6e1a2aefa8a02e1c3855eb77e89
mmzzli <mmzzline@gmail.com>
2025-02-27 17:31:38 +0800

    feat: m3u8 replace to mp4

--- src/utils/video/media.ts
+++ src/utils/video/media.ts
@@ -1,4 +1,4 @@
-
+import { MP4_REGEX } from '../constants'
 export const converMedia = (data: any) => {
   if (!data) return;
   if (data.type === 1) {
@@ -9,7 +9,7 @@ export const converMedia = (data: any) => {
     // const picUrl = medias.find((item) => !item.endsWith('.m3u8'))
     if (data.media && typeof data.media === 'string') {
       const medias = data.media.split(',')
-      const media = medias.find((item: string) => item.endsWith('.m3u8'))
+      const media = medias.find((item: string) => MP4_REGEX.test(item))
       return [media]
     }
   }

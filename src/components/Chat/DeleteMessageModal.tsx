48c614329a54f0b98c832b455152867fa9adf895
gray <516465335@qq.com>
2025-02-28 15:00:46 +0800

    feat: delete msg and send notification

--- src/components/Chat/DeleteMessageModal.tsx
+++ src/components/Chat/DeleteMessageModal.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, useBoolean } from '@chakra-ui/react'
+import { Checkbox, useBoolean, useToast } from '@chakra-ui/react'
 
 import BaseButton from '@/components/BaseButton/BaseButton'
 import { useEffect, useState } from 'react'
@@ -6,8 +6,11 @@ import { useStore } from '@/store/store'
 import { OthersUserInfo } from '@/types'
 import { useDialog } from '@/hooks/useDialog'
 import { Dialog, DialogContent } from '../BaseDialog/BaseDialog'
-import { revokeMsg } from '@/api'
-
+import { deleteMsg, sendDeleteMsgNotification } from '@/api'
+import { CustomToast, typeOptions } from '../comm/Toast'
+import { useTMAUtils } from '@/hooks/useTMAUtils'
+import { Buffer } from 'buffer'
+import { Conversation } from '../SDK/BaeimSDK'
 const sheetStyle: React.CSSProperties = {
   backgroundColor: '#1C1C1C',
   transition: `transform 400ms ease-in-out`,
@@ -142,26 +145,95 @@ export function DeleteMessageDialog({
 }) {
   const [deleteForBoth, setDeleteForBoth] = useState(false)
   const { isOpen, onOpen, onClose } = useDialog()
+  const [pending, setPending] = useState(false)
   const deleteMessage = useStore((state) => state.deleteMessage)
   const setDeleteMessage = useStore((state) => state.setDeleteMessage)
 
-  const handleDelete = async () => {
-    console.log(deleteForBoth)
-    try {
-      // const res = await revokeMsg({
-      //   message_id: deleteMessage?.messageId || '',
-      //   channel_id: deleteMessage?.channelID || '',
-      //   channel_type: 0,
-      // })
-      // console.log(res)
-    } catch (err) {}
+  const { getCurrentUid } = useTMAUtils()
+  const current_uid = getCurrentUid()
+  const toast = useToast()
 
-    // setDeleteMessage(null)
+  const handleDelete = async () => {
+    try {
+      setPending(true)
+      const res = await deleteMsg({
+        message_id: deleteMessage?.messageId || '',
+      })
+      console.log(res)
+      toast({
+        render: () => {
+          return <CustomToast title="Delete" type={typeOptions.success} />
+        },
+        position: 'bottom',
+      })
+      setPending(false)
+
+      // 删除聊天窗口消息
+      const messageWindow = useStore
+        .getState()
+        .messageWindowList.filter((item) => item.channel.channelID === String(receiver?.uid))
+      if (messageWindow.length > 0) {
+        const newMessages = messageWindow[0].messages.filter(
+          (msg) => msg.messageId !== deleteMessage?.messageId
+        )
+        useStore
+          .getState()
+          .updateMessageWindowListItem({ ...messageWindow[0], messages: newMessages })
+      }
+      // 更新会话
+      if (receiver && receiver.uid) {
+        const conversation = useStore.getState().conversationMap[receiver.uid]
+        console.log(conversation)
+        if (conversation) {
+          const recents = conversation.recents?.filter(
+            (recent) => recent.messageID !== deleteMessage?.messageId
+          )
+          const newConversation = {
+            ...conversation,
+            recents,
+            lastMessage: recents ? recents[0] : {},
+          }
+          useStore.getState().updateConversation(newConversation as Conversation)
+        }
+      }
+    } catch (err) {
+      console.log(err)
+      setPending(false)
+    }
+    const buffer = Buffer.from(
+      JSON.stringify({
+        type: 99, // cmd固定type为99
+        cmd: 'DeleteMessage',
+        param: {
+          from: current_uid,
+          message_id: deleteMessage?.messageId,
+          channel_id: String(receiver?.uid),
+          channel_type: 1,
+        },
+      }),
+      'utf-8'
+    )
+    const base64 = buffer.toString('base64')
+    sendDeleteMsgNotification({
+      header: {
+        // 消息头
+        no_persist: 1, // 是否不存储消息 0.存储 1.不存储
+        red_dot: 0, // 是否显示红点计数，0.不显示 1.显示
+        sync_once: 1, // 是否是写扩散，这里一般是0，只有cmd消息才是1
+      },
+      from_uid: String(current_uid), // 发送者uid
+      channel_id: String(receiver?.uid), // 接收频道ID 如果channel_type=1 channel_id为个人uid 如果channel_type=2 channel_id为群id
+      channel_type: 1, // 接收频道类型  1.个人频道 2.群聊频道
+      payload: base64, // 消息内容，base64编码
+      subscribers: [], // 订阅者 如果此字段有值，表示消息只发给指定的订阅者,没有值则发给频道内所有订阅者
+    })
+    setDeleteMessage(null)
     onDelete?.()
   }
 
   const handleCancel = () => {
     setDeleteMessage(null)
+    setPending(false)
   }
 
   useEffect(() => {
@@ -184,7 +256,7 @@ export function DeleteMessageDialog({
             <div className="mt-[24px] text-sm font-normal text-[#666666]">
               {deleteMessage?.receiver === receiver?.uid ? (
                 <div>
-                  <Checkbox
+                  {/* <Checkbox
                     onChange={(e) => {
                       setDeleteForBoth(e.target.checked)
                     }}
@@ -192,7 +264,8 @@ export function DeleteMessageDialog({
                     className="rounded-checkbox"
                   >
                     <span className="text-sm">{`Delete for me and ${receiver?.username}`}</span>
-                  </Checkbox>
+                  </Checkbox> */}
+                  <span className="text-sm">{`Delete for me and ${receiver?.username}`}</span>
                 </div>
               ) : (
                 <span>It will only be removed on your side.</span>
@@ -205,12 +278,18 @@ export function DeleteMessageDialog({
               >
                 Cancel
               </div>
-              <div
+              {/* <div
                 className="cursor-pointer flex items-center justify-center w-[120px] h-[40px] dark:bg-[#FF5330] bg-[#EB4B6D] text-white dark:text-[] rounded-[20px] text-sm"
                 onClick={handleDelete}
               >
                 Delete
-              </div>
+              </div> */}
+              <BaseButton
+                loading={pending}
+                text="Delete"
+                handler={handleDelete}
+                className="cursor-pointer flex items-center justify-center w-[120px] h-[40px] dark:bg-[#FF5330] bg-[#EB4B6D] text-white dark:text-[] rounded-[20px] text-sm"
+              ></BaseButton>
             </div>
           </div>
         </DialogContent>

832442e65a93f0e3129315a68b88da38e4dcde98
jacob <jacob@jacobdeMacBook-Pro.local>
2025-03-01 22:35:37 +0800

    fix: 🐛 fix ui

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ PriceService.init()
 
 // new TomoWalletTgSdkV2({ injected: true, metaData: TOMO_META_DATA.metaData })
 const queryClient = new QueryClient()
-if (import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE === 'dev') {
   mockTelegramEnv({
     themeParams: {
       accentTextColor: '#6ab2f2',
